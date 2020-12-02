55d08626fb1780bfd0ed7dbbb5c788e92a270b20
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2020-12-02 15:13:47 +0800

    update setup

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ function pixiu_prunepath() {
 }
 
 if [ ! -z ${PIXIUPATH:-} ]; then
-    #pixiu_prunepath PATH "$PIXIUPATH"
-    #pixiu_prunepath LD_LIBRARY_PATH "$PIXIUPATH"
-    #pixiu_prunepath DYLD_LIBRARY_PATH "$PIXIUPATH"
+    pixiu_prunepath PATH "$PIXIUPATH"
+    pixiu_prunepath LD_LIBRARY_PATH "$PIXIUPATH"
+    pixiu_prunepath DYLD_LIBRARY_PATH "$PIXIUPATH"
     pixiu_prunepath PYTHONPATH "$PIXIUPATH"
     echo "Cleaned up previously defined PiXiu enviorment"
 fi
@@ -20,4 +20,14 @@ export PIXIUPATH="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -f $PIXIUPATH/python/__init__.py ]; then
   export PYTHONPATH="$PIXIUPATH/python:$PYTHONPATH"
+fi
+
+
+if [ -f $PIXIUPATH/rundir/cxx/src/libPiXiu.so ]; then
+  export PATH="$PIXIUPATH/rundir/cxx/src:$PATH"
+  export PATH="$PIXIUPATH/rundir/src/bin:$PATH"
+  export LD_LIBRARY_PATH="$NCRYSTALDIR/rundir/cxx/src:$LD_LIBRARY_PATH"
+  export DYLD_LIBRARY_PATH="$NCRYSTALDIR/rundir/cxx/src:$DYLD_LIBRARY_PATH"
+else
+  echo "C++ lib is not found"
 fi
