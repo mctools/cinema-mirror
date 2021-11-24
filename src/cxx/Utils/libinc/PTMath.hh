fbe816b1f3518f7c6c9299ca0952efae45cd8fe3
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2021-11-24 12:45:49 +0800

    new math function

--- src/cxx/Utils/libinc/PTMath.hh
+++ src/cxx/Utils/libinc/PTMath.hh
@@ -4,13 +4,17 @@
 #include "PromptCore.hh"
 
 namespace Prompt {
-  bool floateq(double a, double b, double rtol=1e-14, double atol=1e-14);
+  constexpr bool floateq(double a, double b, double rtol=1e-14, double atol=1e-14);
   std::vector<double> logspace(double start, double stop, unsigned num);
   std::vector<double> linspace(double start, double stop, unsigned num);
 
-  double neutronEKin2k(double ekin);
-  double neutronEKin2wl(double ekin);
-  double neutronAngleCosine2Q(double anglecosine, double enin_eV, double enout_eV);
+  constexpr double neutronEKin2k(double ekin);
+  constexpr double neutronAngleCosine2Q(double anglecosine, double enin_eV, double enout_eV);
+  constexpr double wl2ekin( double wl);
+  constexpr double ekin2wl( double ekin);
+  constexpr double wlsq2ekin( double wlsq );
+  constexpr double ekin2wlsq( double ekin);
+  constexpr double ekin2wlsqinv( double ekin);
 }
 
 #include "PTMath.icc"
