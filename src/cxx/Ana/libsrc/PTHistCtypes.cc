843ee7e8606e7dcc94dbaf0e814c40422dc910d6
Yang Ni <970085090@qq.com>
2021-12-20 15:22:55 +0800

    add license to the headers of Ana package

--- src/cxx/Ana/libsrc/PTHistCtypes.cc
+++ src/cxx/Ana/libsrc/PTHistCtypes.cc
@@ -1,5 +1,24 @@
-#include <string>
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  This file is part of Prompt (see https://gitlab.com/xxcai1/Prompt)        //
+//                                                                            //
+//  Copyright 2021-2022 Prompt developers                                     //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//      http://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
 
+#include <string>
 #include "PTHistCtypes.hh"
 #include "PTHist2D.hh"
 #include "PTHist1D.hh"
