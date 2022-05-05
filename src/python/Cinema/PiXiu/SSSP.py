271425cf224104be332e608aa93be7a8cd2b685b
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2022-05-05 15:45:07 +0800

    fix bug

--- src/python/Cinema/PiXiu/SSSP.py
+++ src/python/Cinema/PiXiu/SSSP.py
@@ -4,6 +4,12 @@ from phonopy.interface.vasp import read_vasp
 from phonopy.structure.atoms import atom_data
 import glob
 from .AtomInfo import getAtomMassBC
+from enum import Enum, unique
+
+@unique
+class QEType(Enum):
+    Relax = 0
+    Scf = 1
 
 class Pseudo():
     def __init__(self, accurate=True):
@@ -24,40 +30,79 @@ class Pseudo():
         pseudoName = self.pseudoinfo[elementName]['filename']
         return ecutwfc, ecutrho, pseudoName
 
-    def qems(self, filename, simname, dim, kpt, usePrimitiveCell=True, isMetal=False):
+    def qe_input(self, qeType):
+        if qeType==QEType.Relax:
+            qe_control= """ &control
+                calculation = 'vc-relax'
+                restart_mode='from_scratch'
+                tprnfor = .true.
+                max_seconds = 2570400.0
+                pseudo_dir = {ppath}
+                prefix='out'
+             /
+             &system
+                ibrav = 0
+                nat = {nat}
+                ntyp = {ntyp}
+                {metal}
+                smearing='mv', degauss=0.02
+                ecutwfc = {ecutwfc}, ecutrho={ecutrho}
+                !"vdw-df" "vdw-df2" "rvv10"
+                !input_dft  = 'vdw-df2'
+             /
+             &electrons
+                conv_thr = 1.0d-12
+                mixing_beta = 0.7
+                mixing_mode = 'plain'
+                diagonalization= 'david'
+            /
+            &IONS
+            ion_dynamics= 'bfgs'
+            /
+            &CELL
+           cell_dynamics = 'bfgs'
+             /
+            K_POINTS automatic
+            {kp0} {kp1} {kp2} 0 0 0\n"""
+            return qe_control
+        elif qeType==QEType.Scf:
+            qe_control= """ &control
+                calculation = 'scf'
+                restart_mode='from_scratch'
+                tprnfor = .true.
+                max_seconds = 2570400.0
+                pseudo_dir = {ppath}
+                prefix='out'
+             /
+             &system
+                ibrav = 0
+                nat = {nat}
+                ntyp = {ntyp}
+                {metal}
+                smearing='mv', degauss=0.02
+                ecutwfc = {ecutwfc}, ecutrho={ecutrho}
+                !"vdw-df" "vdw-df2" "rvv10"
+                !input_dft  = 'vdw-df2'
+             /
+             &electrons
+                conv_thr = 1.0d-12
+                mixing_beta = 0.7
+                mixing_mode = 'plain'
+                diagonalization= 'david'
+             /
+            K_POINTS automatic
+            {kp0} {kp1} {kp2} 0 0 0\n"""
+            return qe_control
+
+
+
+    def qems(self, filename, simname, dim, kpt, qeType=QEType.Scf, usePrimitiveCell=True, isMetal=False):
         metal_str="occupations='fixed'"
         if isMetal:
             metal_str="occupations='smearing'"
 
-        qe_contral= """ &control
-            calculation = 'scf'
-            restart_mode='from_scratch'
-            tprnfor = .true.
-            max_seconds = 2570400.0
-            pseudo_dir = {ppath}
-            prefix='out'
-         /
-         &system
-            ibrav = 0
-            nat = {nat}
-            ntyp = {ntyp}
-            {metal}
-            smearing='mv', degauss=0.02
-            ecutwfc = {ecutwfc}, ecutrho={ecutrho}
-            !"vdw-df" "vdw-df2" "rvv10"
-            !input_dft  = 'vdw-df2'
-         /
-         &electrons
-            conv_thr = 1.0d-12
-            mixing_beta = 0.7
-            mixing_mode = 'plain'
-            diagonalization= 'david'
-         /
-        K_POINTS automatic
-        {kp0} {kp1} {kp2} 0 0 0\n"""
-
+        qe_control=self.qe_input(qeType)
         atom_spec ="ATOMIC_SPECIES\n{}"
-
 
         cell = read_vasp(filename)
 
@@ -107,7 +152,7 @@ class Pseudo():
             defatom += elements[i]+' '+str(element_mass[i])+' '+pseudopotentials[i] + "\n"
 
         f=open(simname,'w')
-        f.write(qe_contral.format(ppath="'"+self.libpath+"'",metal=metal_str,nat=tot_ele_num,ntyp=len(elements),kp0=kpt[0]*2,kp1=kpt[1]*2,kp2=kpt[2]*2,ecutwfc=max_ecutwfc, ecutrho=max_ecutrho))
+        f.write(qe_control.format(ppath="'"+self.libpath+"'",metal=metal_str,nat=tot_ele_num,ntyp=len(elements),kp0=kpt[0]*2,kp1=kpt[1]*2,kp2=kpt[2]*2,ecutwfc=max_ecutwfc, ecutrho=max_ecutrho))
         f.write(atom_spec.format(defatom))
         f.write('ATOMIC_POSITIONS crystal\n')
         f.writelines(pos)
@@ -115,27 +160,30 @@ class Pseudo():
         f.writelines(unit_vec)
         f.close()
 
+        suclfiles=None
 
-        if os.system('phonopy --qe -d -v --dim="{dim1} {dim2} {dim3}" '.format(dim1=dim[0],dim2=dim[1],dim3=dim[2])+ " -c " + simname):
-            raise IOError("can't create supercell")
+        if qeType==QEType.Scf:
+            if os.system('phonopy --qe -d -v --dim="{dim1} {dim2} {dim3}" '.format(dim1=dim[0],dim2=dim[1],dim3=dim[2])+ " -c " + simname):
+                raise IOError("can't create supercell")
 
-        suclfiles= glob.glob1('.',"supercel*.in")
-        if len(suclfiles)==0:
-            raise IOError("phononpy didn't created any supercell")
-        print ("generated", len(suclfiles)-1, "supercell files:")
-        print (suclfiles)
-        for sucl in suclfiles:
-            f=open(sucl,'r+')
-            content = f.read()
-            f.seek(0,0)
-            f.write('! input structure file ' + filename + '\n')
-            if usePrimitiveCell:
-                f.write('! primitive cell\n')
-            else:
-                f.write('! conventional cell\n')
-            f.write("! supercell {dim1} {dim2} {dim3}\n".format(dim1=dim[0],dim2=dim[1],dim3=dim[2]) )
-            f.write(f'! kt {kpt[0]} {kpt[1]} {kpt[2]} \n')
-            f.write(qe_contral.format(ppath="'"+self.libpath+"'",metal=metal_str,nat=tot_ele_num*dim[0]*dim[1]*dim[2],ntyp=len(elements),kp0=kpt[0],kp1=kpt[1],kp2=kpt[2],atom=defatom
-    ,ecutwfc=max_ecutwfc, ecutrho=max_ecutrho) + content)
-            f.close()
+            suclfiles= glob.glob1('.',"supercel*.in")
+            if len(suclfiles)==0:
+                raise IOError("phononpy didn't created any supercell")
+            print ("generated", len(suclfiles)-1, "supercell files:")
+            print (suclfiles)
+            for sucl in suclfiles:
+                f=open(sucl,'r+')
+                content = f.read()
+                f.seek(0,0)
+                f.write('! input structure file ' + filename + '\n')
+                if usePrimitiveCell:
+                    f.write('! primitive cell\n')
+                else:
+                    f.write('! conventional cell\n')
+                f.write("! supercell {dim1} {dim2} {dim3}\n".format(dim1=dim[0],dim2=dim[1],dim3=dim[2]) )
+                f.write(f'! kt {kpt[0]} {kpt[1]} {kpt[2]} \n')
+                f.write(qe_control.format(ppath="'"+self.libpath+"'",metal=metal_str,nat=tot_ele_num*dim[0]*dim[1]*dim[2],ntyp=len(elements),kp0=kpt[0],kp1=kpt[1],kp2=kpt[2],atom=defatom
+        ,ecutwfc=max_ecutwfc, ecutrho=max_ecutrho) + content)
+                f.close()
+
         return suclfiles, lattice , positions, elements, tot_ele_num*dim[0]*dim[1]*dim[2]
