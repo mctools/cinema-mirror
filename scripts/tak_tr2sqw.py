9575b9243c1a94975e9eab83d5038bc84de645ff
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2022-05-27 14:49:02 +0800

    protopyte for incoherent and coherent scattering #6

--- scripts/tak_tr2sqw.py
+++ scripts/tak_tr2sqw.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import argparse, scipy.fft, time
-from Cinema.Tak.Analysor import AnaVDOS, AnaSFactor, AnaSF2VD, parFFT
+from Cinema.Tak.Analysor import AnaVDOS, AnaSFactor, AnaSF2VD, parFFT, parFFTs
 import matplotlib.pyplot as plt
 
 #######################################################
@@ -16,29 +16,56 @@ inputfile=args.input
 import h5py
 
 f=h5py.File(inputfile, 'r')
-tr = f['trj'][()]
+tr = f['reduced_trj'][()]
 f.close()
 
-Q=1e-2
+Q=5
 tr = Q*tr
 print(tr.shape)
 
 fftSize = tr.shape[2]
 a=np.zeros(fftSize, dtype=np.complex128)
 b=np.zeros(fftSize)
+
+from numba import jit
+@jit(nopython=True, fastmath=True, parallel=True, cache=True)
+def scaleQ(exponent, factor):
+    return np.exp(-exponent*1j)*factor
+
+@jit(nopython=True, fastmath=True, parallel=True, cache=True)
+def incoherent(b):
+    return (b.real*b.real + b.imag*b.imag).sum(axis=0)
+
+@jit(nopython=True, fastmath=True, parallel=True, cache=True)
+def coherent(b):
+    return b.real.sum(axis=0)**2 + b.imag.sum(axis=0)**2
+
 start = time.time()
 
-for i in range(1, tr.shape[0], 3):
-    # print(i)
-    for dim in range(3):
-        temp = np.fft.fftshift(np.fft.fft(np.exp(-tr[i,dim,:]*1j), n=fftSize))
-        a += temp
-        b += temp.real*temp.real+temp.imag*temp.imag
+# (1536, 3, 25001)
+# inp = np.exp(-tr[0::3,1,:]*1j) #O
+inp = scaleQ(tr, Q) #H
+
+inp = inp.reshape(-1, inp.shape[2])
+print(inp.shape)
+b = parFFT(inp)
+
+###########################################
+inco = incoherent(b)
+inco = np.fft.fftshift(inco)
+
+coh = coherent(b)
+coh = np.fft.fftshift(coh)
+
+#############################################
+
 
 end = time.time()
 print("cal elapsed = %s" % (end - start))
 
 # plt.loglog(a.real*a.real+a.imag*a.imag, label='a')
-plt.semilogy(b, 'o', label='b')
+plt.semilogy(inco, label='inco')
+plt.semilogy(coh, label='coh')
+
 plt.legend()
 plt.show()
