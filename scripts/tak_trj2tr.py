322bd15b882fdbe219a2c6c44d65706311887185
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2022-05-27 09:27:31 +0800

    stucture faction becomes an option #6

--- scripts/tak_trj2tr.py
+++ scripts/tak_trj2tr.py
@@ -11,7 +11,7 @@ parser.add_argument('-i', '--input', action='store', type=str, default='mp-13_Fe
                     dest='input', help='input json file')
 parser.add_argument('-c', '--numcpu', action='store', type=int, default=-1,
                     dest='numcpu', help='number of CPU')
-parser.add_argument('-q', '--numq', action='store', type=int, default=200,
+parser.add_argument('-q', '--numq', action='store', type=int, default=0,
                     dest='numq', help='number of Qpoint')
 parser.add_argument('--test', action='store_true', dest='test', help='run test')
 parser.add_argument('-p','--plot', action='store_true', dest='plot', help='plot figure')
@@ -23,24 +23,25 @@ inputfile=args.input
 numcpu=args.numcpu
 
 s = AnaSFactor(inputfile)
-q, sq = s.getSq(args.numq)
-plt.figure()
-plt.plot(q, sq)
-
+if args.numq:
+    q, sq = s.getSq(args.numq)
+    plt.figure()
+    plt.plot(q, sq)
 
 anavdos = AnaSF2VD(s)
+
+if args.test:
+    vdos = anavdos.vdos(1, numcpu)
+    vdos_python = anavdos.vdos_python(1)
+    np.testing.assert_allclose(vdos_python, vdos)
+    print('pass!')
+    import sys
+    sys.exit()
+
 vdos = anavdos.vdos(1, numcpu)
 if args.plot:
     plt.figure()
     plt.plot(np.abs(vdos), label='C++')
-    plt.show()
-
-if args.test:
-    vdos_python = anavdos.vdos_python(1)
-    plt.figure()
-    plt.plot(np.abs(vdos), label='C++')
-    plt.plot(np.abs(vdos_python), label='python')
-    plt.legend()
     plt.show()
 
 if args.output:
