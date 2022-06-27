7f9c780600f0ee016bb1e6a1377f6fc141916cfb
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2022-06-27 12:07:02 +0800

    fix unittest #12

--- src/pythontests/test_pixiu_powder_readcell.py
+++ src/pythontests/test_pixiu_powder_readcell.py
@@ -3,17 +3,19 @@
 import numpy as np
 from Cinema.PiXiu.PhononCalc import MeshCell
 from Cinema.Interface.Utils import findData
-kt =0.0253 #temperature in kelvin
-#fixme: finddata pacakge
-calc = MeshCell(findData('Al/mesh.hdf5'), findData('Al/cell.json'), kt)
+from Cinema.Interface.units import *
+
+temp =0.0253/boltzmann #temperature in kelvin
+
+calc = MeshCell(findData('Al/mesh.hdf5'), findData('Al/cell.json'), temp)
 
 enSize=3
 QSize=3
 maxQ=1.
 
-reference = np.array([[4.6228779711143622e+11, 1.8611751206224219e+11, 1.9983451195870922e+09],
- [1.6770028159554176e+10, 6.5173703574206064e+12, 1.5761567004272585e+12],
- [0.0000000000000000e+00, 3.7111931199351377e+12, 2.8527382241319131e+12]])
+reference = np.array([[4.6228779711143628e+11, 1.8611751206224219e+11, 1.9983451195870922e+09],
+ [1.6770028159554180e+10, 6.5173703574206064e+12, 1.5761567004272585e+12],
+ [0.0000000000000000e+00, 3.7111931199351382e+12, 2.8527382241319131e+12]])
 
 
 # import profile
