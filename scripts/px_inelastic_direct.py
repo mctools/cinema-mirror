aa6b49a7bc44c63fb253bf1d0a36e39bac9d8bb1
PAN zy <zypan@ihep.ac.cn>
2022-08-15 15:39:38 +0800

    add argparse

--- scripts/px_inelastic_direct.py
+++ scripts/px_inelastic_direct.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+from re import T
 import numpy as np
 from Cinema.PiXiu.PhononCalc import MeshCell, MeshQE
 from Cinema.Interface.Utils import findData
-
+import argparse
 
 #parameters
 #temperature in kelvin
@@ -11,18 +12,29 @@ from Cinema.Interface.Utils import findData
 #frequency bin size for the histogram, freSize, int
 #Q bin size for the histogram, QSize, int
 #stepping for the hkl, int
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--temperature', action='store', type=float, default='',
+                    dest='temp', help='temperature in kelvin', required=True)
+parser.add_argument('-u', '--upper-limit-Q', action='store', type=float, default='10',
+                    dest='maxQ', help='upper limit for the Q')
+parser.add_argument('-f', '--frequency-bin-size', action='store', type=int, default=200,
+                    dest='freSize', help='frequency bin size for the histogram')
+parser.add_argument('-q', '--Q-bin-size', action='store', type=int, default=300,
+                    dest='QSize', help='Q bin size for the histogram')                    
+parser.add_argument('-s', '--step', action='store', type=int, default=1,
+                    dest='step', help='stepping for the hkl')   
+args=parser.parse_args()
 
-temp = 20 #temperature in kelvin
+temp = args.temp #temperature in kelvin
+maxQ = args.maxQ
+freSize = args.freSize
+QSize = args.QSize
+step = args.step
 
 # calc = MeshCell(findData('Al/mesh.hdf5'), findData('Al/cell.json'), kt)
 calc = MeshQE('mesh.hdf5', 'out_relax.xml', temp)
-
-freSize=200
-QSize=300
-maxQ=10
-
 # import profile
-hist = calc.calcPowder(maxQ, freSize, QSize, step=1)
+hist = calc.calcPowder(maxQ, freSize, QSize, step)
 
 hist.save('qehist.h5')
 hist.plot()
--- scripts/px_inelastic_indirect.py
+++ scripts/px_inelastic_indirect.py
@@ -5,26 +5,35 @@ from Cinema.Interface.sctfunc import QeSqw
 import matplotlib.pyplot as plt
 import numpy as np
 from Cinema.Interface.units import hbar
-
+import argparse
 
 #parameters
 #temperature in kelvin
 #scattering angle(scatAngle) in degree
 #energy out in eV
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--temperature', action='store', type=float, required=True,
+                    dest='temp', help='temperature in kelvin')
+parser.add_argument('-a', '--angel-scattering', action='store', type=float, required=True,
+                    dest='scatAngle', help='scattering angle(scatAngle) in degree')
+# 180-135
+# down scattering, as only calculated by px_inelastic_direct.py
+# omega should be negtive, however, it is positive in the calculatoin           
+parser.add_argument('-e', '--energy-out', action='store', type=float, required=True,
+                    dest='enout', help='energy out in eV')         
+args=parser.parse_args()
 
-
+#0.0363271
 #1cm-1=0.00012eV
 #TFXA 135deg 32cm-1 (0.00384eVe), in the book, Vibrational Spectroscopy with Neutrons With Applications in Chemistry, Biology, Materials Science and Catalysis (Philip C. H. Mitchell, Stewart F Parker etc.) page 101
 #TFXA 135deg 24cm-1 (0.00288eV), https://www.isis.stfc.ac.uk/Pages/TOSCA-History.aspx
 #according to ncrystal, graphite 002 d-spacing is 3.3555, as lamba=2*d*sin(135), lambda is 4.7454Aa(i.e. 3.63271meV). It is like the book is correct.
 
-temperature = 20
+temperature = args.temp
+scatAngle = args.scatAngle
+enout = args.enout
 
 sqw = QeSqw('qehist.h5', temperature)
-scatAngle = 180-135
-#down scattering, as only calculated by px_inelastic_direct.py
-# omega should be negtive, however, it is positive in the calculatoin
-enout = 0.0363271
 enin = np.linspace(enout, enout+0.5, 1000)
 sqw.plot(color_order=1e-4)
 
