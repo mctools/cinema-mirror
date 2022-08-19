1614da9d12476b23df81cbb5ef7fd815f1059beb
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2022-08-19 12:34:15 +0800

    remove temperature paramter in the inelastic indirect script

--- scripts/px_inelastic_indirect.py
+++ scripts/px_inelastic_indirect.py
@@ -6,21 +6,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Cinema.Interface.units import hbar
 import argparse
+from Cinema.Interface.helper import plotStyle
+plotStyle()
 
 #parameters
-#temperature in kelvin
 #scattering angle(scatAngle) in degree
 #energy out in eV
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--temperature', action='store', type=float, required=True,
-                    dest='temp', help='temperature in kelvin')
-parser.add_argument('-a', '--angel-scattering', action='store', type=float, required=True,
+parser.add_argument('-a', '--scattering-angel', action='store', type=float, required=True,
                     dest='scatAngle', help='scattering angle(scatAngle) in degree')
 # 180-135
 # down scattering, as only calculated by px_inelastic_direct.py
-# omega should be negtive, however, it is positive in the calculatoin           
+# omega should be negtive, however, it is positive in the calculatoin
 parser.add_argument('-e', '--energy-out', action='store', type=float, required=True,
-                    dest='enout', help='energy out in eV')         
+                    dest='enout', help='energy out in eV')
 args=parser.parse_args()
 
 #0.0363271
@@ -29,7 +28,6 @@ args=parser.parse_args()
 #TFXA 135deg 24cm-1 (0.00288eV), https://www.isis.stfc.ac.uk/Pages/TOSCA-History.aspx
 #according to ncrystal, graphite 002 d-spacing is 3.3555, as lamba=2*d*sin(135), lambda is 4.7454Aa(i.e. 3.63271meV). It is like the book is correct.
 
-temperature = args.temp
 scatAngle = args.scatAngle
 enout = args.enout
 
@@ -44,5 +42,7 @@ plt.plot(q, w)
 plt.figure()
 plt.plot((enin-enout)/0.00012, s)
 plt.grid()
-
+plt.xlabel(r'energy, cm^{-1}')
+plt.ylabel(r'cross section, arb. unit')
+plt.tight_layout()
 plt.show()
