397ebc9642acbc630961ed82fd42ab3f57241995
YangNi20 <970085090@qq.com>
2022-09-19 03:13:30 +0000

    rewrite the classes

--- tools/yn_ana_code.py
+++ tools/yn_ana_code.py
@@ -17,84 +17,74 @@ def atomNum(d2o_r, d2o_h, v_r, v_h): # mm
     v_atomNum=v_volume*v_density/v_molmass*avogadro*v_num
     return d2o_atomNum, v_atomNum
 
-class SQAnaHist1D():
+class WgtFileAnalysor(Hist1D):
+    def __init__(self, xmin, xmax, numbin, xname, weightname, linear=True):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.numbin = numbin
+        self.xname = xname
+        self.linear = linear
+        self. weightname = weightname
+
+    def densityHist(self):
+        x_hist = self.getCentre()
+        y_hist = self.getWeight()/np.diff(self.getEdge())
+        return x_hist, y_hist
+    
+    def fillHist(self, filePath, seedStart, seedEnd, scatnum=None):
+        super().__init__(self.xmin, self.xmax, self.numbin, self.linear)
+        for i in range(seedStart, seedEnd+1):
+            Data = h5py.File(filePath+"/ScororNeutronSq_SofQ_He_seed%d.h5"%i,"r")
+            x = np.array(Data[self.xname])
+            weight = np.array(Data[self.weightname])
+            if scatnum:
+                numScat = np.array(Data['numScat'])
+                idx = np.where(numScat==scatnum)
+                x = x[idx]
+                weight = weight[idx]
+            self.fillmany(x, weight)
+        x_hist, y_hist = self.densityHist()
+        return x_hist, y_hist
+
+class SQTrueHist(WgtFileAnalysor):
+    def __init__(self, xmin, xmax, numbin,linear=True):
+        super().__init__(xmin, xmax, numbin, 'qtrue', 'weight', linear)       
+
+class SQHist(WgtFileAnalysor):
+    def __init__(self, xmin, xmax, numbin,linear=True):
+        super().__init__(xmin, xmax, numbin, 'q', 'weight', linear)
+    
+class MultiScatAnalysor(WgtFileAnalysor):
     def __init__(self, filePath, seedStart, seedEnd):
         self.filePath = filePath
         self.seedStart = seedStart
         self.seedEnd = seedEnd
-    
-    def densityHist(self, hist):
-        x_hist = hist.getCentre()
-        y_hist = hist.getWeight()/np.diff(hist.getEdge())
-        return x_hist, y_hist
         
-    def sqtrueHist(self, xmin, xmax, numbin, linear=True):
-        sqtrue_hist = Hist1D(xmin, xmax, numbin, linear)
+    def sqHist(self, xmin, xmax, numbin, scatnum, inelastic=True, linear=True):
+        if inelastic:
+            super().__init__(xmin, xmax, numbin, 'qtrue', 'weight', linear)
+        else:
+            super().__init__(xmin, xmax, numbin, 'q', 'weight', linear)
+        q_hist, s_hist= self.fillHist(self.filePath, self.seedStart, self.seedEnd, scatnum)
+        return q_hist, s_hist
+    
+    def probaMultiScat(self, scatnum):
+        proba = 0
         for i in range(self.seedStart, self.seedEnd+1):
             Data = h5py.File(self.filePath+"/ScororNeutronSq_SofQ_He_seed%d.h5"%i,"r")
-            qtrue = np.array(Data['qtrue'])
-            weight = np.array(Data['weight'])
-            sqtrue_hist.fillmany(qtrue, weight)   
+            numScat = np.array(Data['numScat'])
+            idx = np.where(numScat==scatnum)
+            weight = np.array(Data['weight'])[idx]
+            proba += np.sum(weight)
+        return proba
+            
+            
         
-        qtrue_hist, s_hist = self.densityHist(sqtrue_hist)
-        return qtrue_hist, s_hist, sqtrue_hist 
 
-    def sqHist(self, xmin, xmax, numbin, linear=True):
-        sq_hist = Hist1D(xmin, xmax, numbin, linear)
-        for i in range(self.seedStart, self.seedEnd+1):
-            Data = h5py.File(self.filePath+"/ScororNeutronSq_SofQ_He_seed%d.h5"%i,"r")
-            q = np.array(Data['q'])
-            weight = np.array(Data['weight'])
-            sq_hist.fillmany(q, weight)   
-        
-        q_hist, s_hist = self.densityHist(sq_hist)
-        return q_hist, s_hist, sq_hist 
 
-class MultiScatAna(SQAnaHist1D):
-    def __init__(self, filePath, seedStart, seedEnd, scatnum_max):
-        super().__init__(filePath, seedStart, seedEnd)
-        self.scatnum_max = scatnum_max
-    
-    def sqtrueHist(self, xmin, xmax, numbin, linear=True):
-        s_multiscat = np.zeros((self.scatnum_max,numbin), dtype=float)
-        for i in range(1, self.scatnum_max+1):
-            sqtrue_hist = Hist1D(xmin, xmax, numbin, linear)
-            for j in range(self.seedStart, self.seedEnd+1):
-                Data = h5py.File(self.filePath+'/ScororNeutronSq_SofQ_He_NumScat%d_seed%d.h5'%(i, j),"r")
-                qtrue = np.array(Data['qtrue'])
-                weight = np.array(Data['weight'])
-                sqtrue_hist.fillmany(qtrue, weight)
-            qtrue_hist, s_hist = self.densityHist(sqtrue_hist) 
-            s_multiscat[i-1,:] = s_hist
-        qtrue_multiscat = qtrue_hist
-        return qtrue_multiscat, s_multiscat
 
-    def sqHist(self, xmin, xmax, numbin, linear=True):
-        s_multiscat = np.zeros((self.scatnum_max,numbin), dtype=float)
-        for i in range(1, self.scatnum_max+1):
-            sq_hist = Hist1D(xmin, xmax, numbin, linear)
-            for j in range(self.seedStart, self.seedEnd+1):
-                Data = h5py.File(self.filePath+'/ScororNeutronSq_SofQ_He_NumScat%d_seed%d.h5'%(i, j),"r")
-                q = np.array(Data['q'])
-                weight = np.array(Data['weight'])
-                sq_hist.fillmany(q, weight)
-            q_hist, s_hist = self.densityHist(sq_hist) 
-            s_multiscat[i-1,:] = s_hist
-        q_multiscat = q_hist
-        return q_multiscat, s_multiscat
 
-    def probaMultiScat(self):
-        scatnum = np.zeros((self.scatnum_max), dtype=int)
-        scatnum_proba = np.zeros(shape=(self.scatnum_max), dtype=float)
-        for i in range(1, self.scatnum_max+1):
-            scatnum[i-1] = i
-            proba = 0
-            for j in range(self.seedStart, self.seedEnd+1):
-                Data = h5py.File(self.filePath+'/ScororNeutronSq_SofQ_He_NumScat%d_seed%d.h5'%(i, j),"r")
-                weight = np.array(Data['weight'])
-                proba += np.sum(weight)
-            scatnum_proba[i-1] = proba
-        return scatnum, scatnum_proba
+
 
         
        
