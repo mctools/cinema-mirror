mctools/cinema-mirror

commit e0ec43e3b96e558440c46c7bc41f2a3330c472a1

simplify the top layer interface

src/cxx/Geometry/libsrc/PTNavManager.cc
@@ -212,10 +212,7 @@ bool Prompt::NavManager::proprogateInAVolume(Particle &particle, bool verbose )
     //sample the interaction at the location
     double final_ekin(0);
     Vector final_dir;
-    m_matphysscor->physics->sampleFinalState(particle, final_ekin, final_dir);
-    // final reaction channel is picked here, calculate the weigth factor
-    // based on individual xs, stepLength and picked physics
-    particle.scaleWeight( m_matphysscor->physics->calculateWeight(step, true));
+    m_matphysscor->physics->sampleFinalState(particle, step, false);
     // std::cout << particle.getEventID() << ", particle  weight " << particle.getWeight() <<std::endl;
 
     if(final_ekin==-1.) // fixme: are we sure all -1 means capture??
@@ -236,9 +233,7 @@ bool Prompt::NavManager::proprogateInAVolume(Particle &particle, bool verbose )
     double final_ekin(0);
     Vector final_dir;
 
-    m_matphysscor->physics->sampleFinalState(particle, final_ekin, final_dir);
-    particle.scaleWeight(m_matphysscor->physics->calculateWeight(step, false));
-    // std::cout << particle.getEventID() << ", particle  weight " << particle.getWeight() <<std::endl;
+    m_matphysscor->physics->sampleFinalState(particle, step, true);
     return false;
   }
 

src/cxx/Material/libsrc/PTMaterialPhysics.cc
@@ -35,9 +35,15 @@ double Prompt::MaterialPhysics::macroCrossSection(const Prompt::Particle &partic
   return m_numdensity*m_compModel->totalCrossSection(particle.getEKin(), particle.getDirection());
 }
 
-void Prompt::MaterialPhysics::sampleFinalState(const Prompt::Particle &particle, double &final_ekin, Vector &final_dir)
+void Prompt::MaterialPhysics::sampleFinalState(Prompt::Particle &particle, double stepLength, bool hitWall) const
 {
-  m_compModel->sample(particle.getEKin(), particle.getDirection(), final_ekin, final_dir);
+  double final_ekin;
+  Vector final_dir;
+  m_compModel->generate(particle.getEKin(), particle.getDirection(), final_ekin, final_dir);
+  particle.setEKin(final_ekin);
+  particle.setDirection(final_dir);
+  if(!stepLength)
+    particle.scaleWeight(m_compModel->calculateWeight(stepLength*m_numdensity, hitWall));
 }
 
 double Prompt::MaterialPhysics::sampleStepLength(const Prompt::Particle &particle) const
@@ -53,10 +59,6 @@ double Prompt::MaterialPhysics::sampleStepLength(const Prompt::Particle &particl
   }
 }
 
-double Prompt::MaterialPhysics::calculateWeight(double step, bool selBiase)
-{
-  return m_compModel->calculateWeight(step*m_numdensity, selBiase);
-}
 
 double Prompt::MaterialPhysics::calNumDensity(const std::string &cfg)
 {

src/cxx/Material/libinc/PTMaterialPhysics.hh
@@ -33,8 +33,7 @@ namespace Prompt {
     virtual ~MaterialPhysics();
 
     double sampleStepLength(const Prompt::Particle &particle) const;
-    void sampleFinalState(const Prompt::Particle &particle, double &final_ekin, Vector &final_dir);
-    double calculateWeight(double step, bool selBiase);
+    void sampleFinalState(Prompt::Particle &particle, double stepLength=0., bool hitWall=false) const;
     void addComposition(const std::string &cfg, double bias=1.0);
 
   private:

src/cxx/Models/libinc/PTCompoundModel.hh
@@ -51,12 +51,12 @@ namespace Prompt {
     virtual ~CompoundModel();
 
     double totalCrossSection(double ekin, const Vector &dir) const;
-    void sample(double ekin, const Vector &dir, double &final_ekin, Vector &final_dir) const;
 
     void addPhysicsModel(const std::string &cfg, double bias=1.);
     bool sameInquiryAsLastTime(double ekin, const Vector &dir) const;
 
-    double calculateWeight(double lengthRho, bool selBiase);
+    void generate(double ekin, const Vector &dir, double &final_ekin, Vector &final_dir) const;
+    double calculateWeight(double lengthRho, bool hitWall);
 
   private:
     std::vector<std::shared_ptr<DiscreteModel> > m_models;

src/cxx/Models/libsrc/PTCompoundModel.cc
@@ -75,7 +75,7 @@ double Prompt::CompoundModel::totalCrossSection(double ekin, const Vector &dir) 
   }
 }
 
-void Prompt::CompoundModel::sample(double ekin, const Vector &dir, double &final_ekin, Vector &final_dir) const
+void Prompt::CompoundModel::generate(double ekin, const Vector &dir, double &final_ekin, Vector &final_dir) const
 {
   if(!sameInquiryAsLastTime(ekin, dir))
     printf("WARNING, sampling event with different incident energy and/or direction\n");
@@ -104,18 +104,18 @@ void Prompt::CompoundModel::sample(double ekin, const Vector &dir, double &final
 }
 
 //this shoule be called right after cross section is updated
-double Prompt::CompoundModel::calculateWeight(double lengthRho, bool selBiase)
+double Prompt::CompoundModel::calculateWeight(double lengthRho, bool hitWall)
 {
   double factor(1.);
   for(size_t i=0;i<m_models.size();i++)
   {
     double modbias(m_models[i]->getBias());
     if (modbias==1.) continue;
+    //this factor contributed in this step of lengthRho by this model
     factor *= exp( (m_cache.bias[i]-1.)*lengthRho* m_cache.cache_xs[i]/m_cache.bias[i] );
-    // std::cout << "exponet " <<  (m_cache.bias[i]-1.)*lengthRho* m_cache.cache_xs[i] <<
-    // " "<< exp( (m_cache.bias[i]-1.)*lengthRho* m_cache.cache_xs[i] ) << std::endl;
   }
   // std::cout << "selectedBias " << m_cache.selectedBias << " factor " << factor
   // << std::endl;
-  return (m_cache.selectedBias!=1. && selBiase ) ?  (factor/m_cache.selectedBias) : factor;
+  assert(m_cache.selectedBias);
+  return (m_cache.selectedBias!=1. && !hitWall ) ?  (factor/m_cache.selectedBias) : factor;
 }

src/cxxtests/unittest/test_modelcollection.cc
@@ -38,12 +38,12 @@ TEST_CASE("CompoundModel")
   CHECK(Prompt::floateq(1.378536096609809*Prompt::Unit::barn, xs ));
 
 
-  collection.sample(1., {1,0,0}, final, out);
+  collection.generate(1., {1,0,0}, final, out);
   std::cout << final << " " << out << std::endl;
 
-  collection.sample(1., {1,0,0}, final, out);
+  collection.generate(1., {1,0,0}, final, out);
   std::cout << final << " " << out << std::endl;
 
-  collection.sample(1., {1,0,0}, final, out);
+  collection.generate(1., {1,0,0}, final, out);
   std::cout << final << " " << out << std::endl;
 }

commit 37f53ad05ea06608501b5fa2a38e1f480506c93e

fix a major bug that introduced in commit e0ec43e3b96e558440c46c7bc41f2a3330c472a1

src/cxx/Geometry/libsrc/PTNavManager.cc
@@ -204,31 +204,18 @@ bool Prompt::NavManager::proprogateInAVolume(Particle &particle, bool verbose )
   if(sameVolume)
   {
     //sample the interaction at the location
-    double final_ekin(0);
-    Vector final_dir;
     m_matphysscor->bulkPhysics->sampleFinalState(particle, step, false);
     // std::cout << particle.getEventID() << ", particle  weight " << particle.getWeight() <<std::endl;
 
-    if(final_ekin==-1.) // fixme: are we sure all -1 means capture??
+    if(particle.getEKin()==-1.) // fixme: are we sure all -1 means capture??
     {
       particle.kill(Particle::KillType::ABSORB);
-    }
-    else
-    {
-      particle.setEKin(final_ekin);
-      particle.setDirection(final_dir);
     }
     return true;
   }
   else
   {
-    // final reaction channel is picked here, calculate the weigth factor
-    // based on individual xs, stepLength and picked physics
-    double final_ekin(0);
-    Vector final_dir;
-
     m_matphysscor->bulkPhysics->sampleFinalState(particle, step, true);
     return false;
   }
-
 }

src/cxx/Material/libsrc/PTMaterialPhysics.cc
@@ -40,11 +40,16 @@ void Prompt::MaterialPhysics::sampleFinalState(Prompt::Particle &particle, doubl
   double final_ekin;
   Vector final_dir;
   m_compModel->generate(particle.getEKin(), particle.getDirection(), final_ekin, final_dir);
-  particle.setEKin(final_ekin);
-  particle.setDirection(final_dir);
-  if(!stepLength)
+  if(!hitWall)
+  {
+    particle.setEKin(final_ekin);
+    particle.setDirection(final_dir);
+  }
+
+  if(stepLength)
     particle.scaleWeight(m_compModel->calculateWeight(stepLength*m_numdensity, hitWall));
 }
+
 
 double Prompt::MaterialPhysics::sampleStepLength(const Prompt::Particle &particle) const
 {