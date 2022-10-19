3f3f45fdf058c89171c62ac3660179a908241875
YangNi20 <970085090@qq.com>
2022-10-19 13:15:11 +0000

    update test_prompt_gun.py

--- src/pythontests/test_prompt_gun.py
+++ src/pythontests/test_prompt_gun.py
@@ -2,24 +2,22 @@
 
 import numpy as np
 import os
+from Cinema.Prompt import PromptFileReader
 
 filelist=['SimpleThermalGun.gdml', 'IsotropicGun.gdml', 'UniModeratorGun.gdml', 'MPIGun.gdml', 'MaxwellianGun.gdml']
 valuelist=[[5894.344942918124, 1895., 1895.], [5894.344942918124, 5406., 5406.], [ 5894.344942918124, 78823., 78823.], [5894.344942918124, 86395., 86395.], [5894.344942918124, 83811., 83811.]]
-f1='ScorerNeutronSq_SofQ_seed103_edge.npy'
-f2='ScorerNeutronSq_SofQ_seed103_content.npy'
-f3='ScorerNeutronSq_SofQ_seed103_hit.npy'
+f1='ScorerNeutronSq_SofQ_seed103.mcpl.gz'
 
 for i in range(0,5):
     os.system(f'rm {f1}')
-    os.system(f'rm {f2}')
-    os.system(f'rm {f3}')
     os.system('prompt -g %s -s 103 -n 1e5'%filelist[i])
 
-    x=np.load(f1)
-    w=np.load(f2)
-    hit=np.load(f3)
+    f = PromptFileReader(f1)
+    hist_weight = f.getData('content').sum()
+    hist_hit = f.getData('hit').sum()
+    hist_edge = f.getData('edge').sum()
 
-    res = np.array([x.sum(), w.sum(), hit.sum()])
+    res = np.array([hist_edge, hist_weight, hist_hit])
     np.set_printoptions(precision=16)
     print(res)
     np.testing.assert_allclose(res, valuelist[i], rtol=1e-15)
