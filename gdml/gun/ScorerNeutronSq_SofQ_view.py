f0c48177d40dd29314a06cae6beb0b470c3b1086
YangNi20 <970085090@qq.com>
2022-10-19 12:30:19 +0000

    delete useless data files

--- gdml/gun/ScorerNeutronSq_SofQ_view.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x=np.load('ScorerNeutronSq_SofQ_seed4096_edge.npy')
-y=np.load('ScorerNeutronSq_SofQ_seed4096_content.npy')
-plt.loglog(x[:-1],y/np.diff(x), label=f'integral={y.sum()}')
-plt.grid()
-plt.legend()
-plt.show()
