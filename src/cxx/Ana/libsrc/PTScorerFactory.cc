2ace3bb8017e5a006185c33c5ac010489a2fba19
YangNi20 <970085090@qq.com>
2022-10-27 06:58:55 +0000

    add comments of scorer names

--- src/cxx/Ana/libsrc/PTScorerFactory.cc
+++ src/cxx/Ana/libsrc/PTScorerFactory.cc
@@ -123,6 +123,7 @@ std::shared_ptr<Prompt::Scorer> Prompt::ScorerFactory::createScorer(const std::s
 
     else if(ScorDef == "PSD")
     {
+      // PSD: position sensitive detector
 
       int parCount = 9;
 
@@ -169,8 +170,10 @@ std::shared_ptr<Prompt::Scorer> Prompt::ScorerFactory::createScorer(const std::s
 
     else if(ScorDef == "ESpectrum")
     {
+      // ESpectrum: energy spectrum
       // example cfg
       // ""Scorer=ESD; name=detector; Emin=0.0; Emax=0.0253; numbin=100""
+
       int parCount = 5;
 
       // The mandatory parameters
@@ -189,8 +192,10 @@ std::shared_ptr<Prompt::Scorer> Prompt::ScorerFactory::createScorer(const std::s
     }
     else if(ScorDef == "TOF")
     {
+      // TOF: time-of-flight
       // example cfg
       // ""Scorer=TOF; name=detector; Tmin=0.0; Tmax=0.5; numbin=1000""
+
       int parCount = 5;
 
       // The mandatory parameters
@@ -209,9 +214,11 @@ std::shared_ptr<Prompt::Scorer> Prompt::ScorerFactory::createScorer(const std::s
     }
     else if(ScorDef == "MultiScat")
     {
+      // MultiScat: multiple scattering
       // example cfg
       // ""Scorer=MultiScat; name=D2O; Numbermin=1; Numbermax=5; linear=yes""
       // the default value for linear is yes
+
       int parCount = 5;
 
       // The mandatory parameters
@@ -248,6 +255,8 @@ std::shared_ptr<Prompt::Scorer> Prompt::ScorerFactory::createScorer(const std::s
     }
     else if(ScorDef == "VolFlux")
     {
+      // VolFlux: volume flux
+      
       int parCount = 6;
 
       bool force = true;
