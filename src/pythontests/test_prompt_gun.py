7d69baa630c5cac0c6b82de4c78ad95e6b834d9e
YangNi20 <970085090@qq.com>
2022-11-07 06:38:32 +0000

    particle number is changed to 1e3

--- src/pythontests/test_prompt_gun.py
+++ src/pythontests/test_prompt_gun.py
@@ -5,13 +5,12 @@ import os
 from Cinema.Prompt import PromptFileReader
 
 filelist=['SimpleThermalGun.gdml', 'IsotropicGun.gdml', 'UniModeratorGun.gdml', 'MPIGun.gdml', 'MaxwellianGun.gdml']
-valuelist=[[5894.344942918124, 1895., 1895.], [5894.344942918124, 5406., 5406.], [ 5894.344942918124, 78823., 78823.], [5894.344942918124, 86395., 86395.], [5894.344942918124, 83811., 83811.]]
+valuelist=[[5894.344942918124, 14., 14.], [5894.344942918124, 55., 55.], [5894.344942918124, 790., 790.], [5894.344942918124, 855., 855.], [5894.344942918124, 850., 850.]]
 f1='ScorerNeutronSq_SofQ_seed103.mcpl.gz'
 
 for i in range(0,5):
     os.system(f'rm {f1}')
-    os.system('prompt -g %s -s 103 -n 1e5'%filelist[i])
-
+    os.system('prompt -g %s -s 103 -n 1e3'%filelist[i])
     f = PromptFileReader(f1)
     hist_weight = f.getData('content').sum()
     hist_hit = f.getData('hit').sum()
