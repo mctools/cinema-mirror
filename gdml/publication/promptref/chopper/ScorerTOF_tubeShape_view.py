419dcae761590704336caaa6da95f4a28586718b
ziyipan <12010757+ziyipan@user.noreply.gitee.com>
2023-02-28 09:14:48 +0800

    check chopper shape

---
+++ gdml/publication/promptref/chopper/ScorerTOF_tubeShape_view.py
@@ -0,0 +1,17 @@
+import numpy as np
+from Cinema.Prompt import PromptFileReader
+import matplotlib.pyplot as plt
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--linear', action='store_true', dest='logscale', help='colour bar in log scale')
+args=parser.parse_args()
+f = PromptFileReader('ScorerTOF_tubeShape_seed4096.mcpl.gz')
+x=f.getData('edge')
+y=f.getData('content')
+if args.logscale:
+  plt.semilogy(x[:-1],y/np.diff(x), label=f'total weight={y.sum()}')
+else:
+  plt.plot(x[:-1],y/np.diff(x), label=f'total weight={y.sum()}')
+plt.grid()
+plt.legend()
+plt.show()
