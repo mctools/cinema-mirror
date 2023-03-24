a0c6382ed34875d3152cacb0f0f0d35fe31afd51
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2023-03-24 11:15:11 +0800

    update pus plot script

--- gdml/publication/promptref/powder_diffaraction/simulation_pus_plot.py
+++ gdml/publication/promptref/powder_diffaraction/simulation_pus_plot.py
@@ -5,23 +5,28 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import glob
 import sys, os
+plotStyle()
 
 pusData = np.loadtxt('PUS_data.dat')
-simuData = np.loadtxt('prompt_data.dat')
+f = PromptFileReader(f'ScorerNeutronSq_ST_template_seed4096.mcpl.gz')
+x=f.getData('edge')
+x=x[:-1]+np.diff(x)*0.5
+y=f.getData('content')
 
-plotStyle()
+
 fig, ax = plt.subplots(1,1)
-ax.yaxis.set_major_locator(ticker.MultipleLocator(2000))
+# ax.yaxis.set_major_locator(ticker.MultipleLocator(2000))
 
-l2, = plt.plot(pusData[:,0], pusData[:,1]*5, 'ko')
-l1, = plt.plot(simuData[:,0], simuData[:,1], 'r')
+l2, = plt.plot(pusData[:,0], pusData[:,1]/pusData[:,1].sum()*100, 'ko')
+l1, = plt.plot(x-0.22, y/y.sum()/1.38*100, 'r')
 
-plt.ylim(0,1.1)
-plt.xlim(0,130.1)
+# plt.ylim(0,1.1)
+# plt.xlim(0,130.1)
+plt.grid()
+
 plt.xlabel("scattering angle, deg")
 plt.ylabel("Count rate, arb. unit")
-plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
-plt.legend([l1, l2], ['Prompt NCrystal simulated', 'IFE PUS measured'])
-plt.grid()
+plt.legend([l1, l2], ['Simulated', r'PUS measured@1.5549$\AA$ '])
+
 plt.tight_layout()
 plt.show()
