fix bug in error calculation

src/python/Cinema/Experiment/Analyser/DataLoader.py
@@ -84,7 +84,7 @@ class ErrorPropagator():
         otherw = weight
         othererr = np.divide(error, otherw, where=(otherw!=0.))
         self.weight = np.divide(self.weight, otherw, where=(otherw!=0.))
-        self.error = np.sqrt(selferr*selferr + othererr*othererr)*self.weight
+        self.error = np.sqrt(selferr*selferr + othererr*othererr)*np.abs(self.weight)
 
     def __itruediv__(self, other):
         if hasattr(other, 'ycentre'):
@@ -95,7 +95,7 @@ class ErrorPropagator():
 
     def scale(self, factor):
         self.weight = self.weight*factor
-        self.error = self.error*factor
+        self.error = self.error*np.abs(factor)
 
 
     def plot(self, show=False, label=None, idx = 2000):