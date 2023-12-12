7e7c303faf442741b2b709c22b4ff346c29541dd
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2023-12-12 14:52:11 +0800

    update guide.py example

--- scripts/guide.py
+++ scripts/guide.py
@@ -8,6 +8,7 @@ from Cinema.Prompt.gun import PythonGun
 from Cinema.Prompt.Histogram import wl2ekin
 from Cinema.Prompt.physics import Material, Mirror
 from Cinema.Prompt.component import makeTrapezoidGuide
+from Cinema.Prompt.gun import UniModeratorGun
 
 import numpy as np
 
@@ -18,7 +19,11 @@ class MySim(PromptMPI):
         super().__init__(seed)   
 
     def makeWorld(self):
-        world = Volume("world", Box(70, 70, 2100))
+        universe = Material('freegas::H1/1e-26kgm3')
+        universe.setBiasScat(1.)
+        universe.setBiasAbsp(1.)
+
+        world = Volume("world", Box(70, 70, 2100), matCfg=universe)
 
         detector1 = Volume("det1", Box(25, 25, 0.0001))
         makePSD('psd1', detector1, 20, 20 )
@@ -27,6 +32,10 @@ class MySim(PromptMPI):
         detector2 = Volume("det2", Box(35, 35, 0.0001))
         makePSD('psd2', detector2, 20, 20 )
         world.placeChild("det2", detector2, Transformation3D(0., 0., 1200))
+
+        ESpectrumHelper('espec').make(detector2)
+        WlSpectrumHelper('wlspec').make(detector2)
+        TOFHelper('tof').make(detector2)
 
         guide = makeTrapezoidGuide(500, 25,25,25,25, 1.0, 10)
         world.placeChild("guide", guide, Transformation3D(0., 0., -400))
@@ -38,19 +47,25 @@ class MySim(PromptMPI):
 sim = MySim(seed=1010)
 sim.makeWorld()
 
-# set gun
-gunCfg = f'gun=UniModeratorGun;mean_wl=3.39;range_wl=0.3;src_w=50;src_h=50;src_z=-2000;slit_w=50;slit_h=50;slit_z=-1000"'
+
+gun = UniModeratorGun()
+gun.setWlMean(3.39)
+gun.setWlRange(0.3)
+gun.setSlit([50,50,-1000])
+gun.setSource([50,50,-2000])
+
 
 # vis or production
 if False:
     sim.show(1)
 else:
-    sim.simulate(gunCfg, 1e6)
+    sim.simulate(gun, 1e6)
 
 destination = 0
-psd1 = sim.gatherHistData('psd1')
-psd2 = sim.gatherHistData('psd2')
-
+psd1 = sim.gatherHistData('psd1', dst=destination)
+psd2 = sim.gatherHistData('psd2', dst=destination)
+wlspec = sim.gatherHistData('wlspec', dst=destination)
 if sim.rank==destination:
-    psd1.plot(show=False, log=False)
+    psd1.plot(show=True, log=False)
     psd2.plot(show=True, log=False)
+    wlspec.plot(show=True, log=False)
