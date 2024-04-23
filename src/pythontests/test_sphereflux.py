461468dead4ccfc09f405fba33f5d1af0c7cbf5c
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2024-04-23 14:39:14 +0800

    fix unittest

--- src/pythontests/test_sphereflux.py
+++ src/pythontests/test_sphereflux.py
@@ -6,7 +6,10 @@ from Cinema.Prompt.solid import Box, Sphere
 from Cinema.Prompt.scorer import VolFluenceHelper
 from Cinema.Prompt.physics import Material
 from Cinema.Prompt.gun import IsotropicGun
+from Cinema.Prompt.centralData import CentralData 
 
+cdata=CentralData()
+cdata.setEnableGidi(True)
 
 
 class MySim(Prompt):
@@ -18,7 +21,7 @@ class MySim(Prompt):
         world = Volume("world", Box(400, 400, 400))
 
         # lw = Material("'LiquidWaterH2O_T293.6K.ncmat;density=1gcm3'") # v2, 4.630884342994241. ml, 4.630884342994241. full spec 4.691033674854935 should 4.7046
-        lw = Material('freegas::H2O/1gcm3/H_is_1.00_H1/O_is_1.00_O16') # v2, 4.5781432733636525. ml, 4.5781432733636525. full spec 4.638707241003883, should be 4.6490. gidi 3.615059805936363                                           
+        lw = Material('freegas::H2O/1gcm3/H_is_H1/O_is_O16') # v2, 4.5781432733636525. ml, 4.5781432733636525. full spec 4.638707241003883, should be 4.6490. gidi 3.615059805936363                                           
 
         lw.setBiasScat(1.)
         lw.setBiasAbsp(1.)
@@ -35,9 +38,9 @@ sim.makeWorld()
 
 
 gun = IsotropicGun()
-gun.setEnergy(10e6)
+gun.setEnergy(1)
 
-partnum = 10
+partnum = 100
 # vis or production
 sim.simulate(gun, partnum)
 
@@ -47,6 +50,6 @@ print(spct.getWeight().sum())
 # spct.plot(show=True, log=True)
 
 import numpy.testing as npt
-npt.assert_array_almost_equal(spct.getWeight().sum(), 7.086587633249583e-05, decimal =  15)
+npt.assert_array_almost_equal(spct.getWeight().sum(), 0.00040921634486766265, decimal =  15)
 
 
