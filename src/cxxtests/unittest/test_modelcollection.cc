4b1a94064faee2b32a90455a42a75fd1e6e74ace
panzy <zypan@ihep.ac.cn>
2024-05-15 13:37:52 +0800

    add compound model test

--- src/cxxtests/unittest/test_modelcollection.cc
+++ src/cxxtests/unittest/test_modelcollection.cc
@@ -21,9 +21,47 @@
 #include "../doctest.h"
 #include "PTMath.hh"
 #include "PTModelCollection.hh"
+#include "PTParticleProcess.hh"
 
 TEST_CASE("ModelCollection")
 {
+  std::vector<double> expectedEnergyOut;
+  expectedEnergyOut = {0.0253, 0.0263, 0.045192303408606, 0.0283, -1, 0.00542322400666893 };
+  std::vector<std::vector<double>> expectedDirOut;
+  expectedDirOut ={
+    { 0.211330297846913, -0.592201916881118, -0.777583689935858 },
+    { -0.896705652516454, 0.227612286342529, -0.379620362798107 },
+    { 0.349974039965383, -0.141170718428235, 0.926061012897511 },
+    { -0.762662850218472, 0.547355775591302, -0.344596912092202 }, 
+    { -0.762662850218472, 0.547355775591302, -0.344596912092202 }, //same as previous one due to absorption, variable outdir unchanged
+    { -0.423603163886187, -0.904003071387581, 0.0577824062101281 }
+  };
+  double inE(0.0253);
+  auto inDir = Prompt::Vector({1.,0.,0.});
+  auto pp = Prompt::ParticleProcess("Al_sg225.ncmat");
+  auto compModel = pp.getModelCollection();
+  double xs(0.);
+  std::cout.precision(15);
+  std::cout << "Total XS: " << xs/Prompt::Unit::barn << std::endl;
+
+  double finE(0.);
+  Prompt::Vector finDir;
+  for(size_t i;i<6;i++)
+  {
+    std::cout << "In energy: " << inE << std::endl;
+    std::cout << "In dir: " << inDir << std::endl;
+    xs = compModel->totalCrossSection(2112, inE, inDir);
+    std::cout << "Total XS: " << xs/Prompt::Unit::barn << std::endl;
+    compModel->generate(inE, inDir, finE, finDir);
+    std::cout << "Out energy: " << finE << std::endl;
+    CHECK(Prompt::floateq(finE, expectedEnergyOut[i]));
+    std::cout << "Out dir: " << finDir << std::endl;
+    CHECK(Prompt::floateq(finDir.x(), expectedDirOut[i][0]));
+    CHECK(Prompt::floateq(finDir.y(), expectedDirOut[i][1]));
+    CHECK(Prompt::floateq(finDir.z(), expectedDirOut[i][2]));
+    std::cout << "  " << std::endl;
+    inE += 0.001;
+  }
 
   // auto collection = Prompt::ModelCollection(2112) ;
   // collection.addNCScaAbsModels("Al_sg225.ncmat;dcutoff=0.5;temp=25C");
