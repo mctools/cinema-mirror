456104809599e95d76afc7e9b94dec90925a1f03
panzy <zypan@ihep.ac.cn>
2024-08-08 10:38:45 +0800

    base where sw matches

--- scripts/pt_convolution.py
+++ scripts/pt_convolution.py
@@ -34,20 +34,22 @@ class MySim(Prompt):
     def __init__(self, seed=4096) -> None:
         super().__init__(seed)   
 
-    def makeWorld(self):
+    def makeWorld(self, w=0.01):
 
         world = Volume("world", Box(200, 200, 400))
 
-        lw = "physics=idealElaScat;xs_barn=1;density_per_aa3=5;energy_transfer_eV=0.02"
+        lw = f"physics=idealElaScat;xs_barn=1;density_per_aa3=5;energy_transfer_eV={w}"
         # lw = Material('solid::CH2/1gcm3')
 
         monitor = Volume("mon", Box(1,1,1.e-6))
         eRange = [0., 0.2]
         binNum = (eRange[1] - eRange[0]) * 1000   #0.001 per bin
-        ESpectrumHelper('incident_energy', eRange[0], eRange[1], int(binNum), groupID=0, linear=True).make(monitor)
-        world.placeChild('monPV', monitor, Transformation3D(0.,0.,-50))
+        ESpectrumHelper('incident_energy', eRange[0], eRange[1], int(binNum), groupID=1, linear=True).make(monitor)
+        ESpectrumHelper('incident_energy_exit', eRange[0], eRange[1], int(binNum), groupID=2, linear=True).make(monitor)
+        world.placeChild('monPV', monitor, Transformation3D(0.,0.,-50), scorerGroup=1)
+        world.placeChild('monPV_exit', monitor, Transformation3D(0.,0.,190), scorerGroup=2)
 
-        sample = Volume("sample", Box(1,1,1), matCfg=lw)
+        sample = Volume("sample", Box(1.e-6,1.e-6,1), matCfg=lw)
         ms = MultiScatCounter()
         ms.make(sample)
         world.placeChild('samplePV', sample)
@@ -55,36 +57,50 @@ class MySim(Prompt):
 
         detMat = Material("freegas::He/0.0001347gcm3/He_is_He3") # Employ this (and also ABSORB pts) to boarden energy spectrum by detector efficiency
         detector = Volume("DttLV", Sphere(50, 51, 0 , 2*np.pi, starttheta=np.deg2rad(1), deltatheta=np.pi-np.deg2rad(2)))
-        dttSc = ESpectrumHelper('energy_gp0', eRange[0], eRange[1], int(binNum), ptstate='ENTRY', energyTransfer=True, groupID=0, linear=True)
+        dttSc = ESpectrumHelper('energy_gp0', eRange[0], eRange[1], int(binNum*0.1)+1, ptstate='ENTRY', energyTransfer=True, groupID=0, linear=True)
         dttSc.make(detector)
-        dttSc.addScatterCounter(ms, 1)
+        dttSc.addScatterCounter(ms, -1)
         world.placeChild('DttPV', detector)
 
         self.setWorld(world)
 
-
-
+w_axi = []
+i_value = []
 sim = MySim(seed=1010)
-sim.makeWorld()
+for w in np.arange(0., 0.12, 0.04):
+    sim.clear()
+    sim.makeWorld(w)
 
-gun = MyGun(2112, [0.11, 0.12])
+    gun = MyGun(2112, [0.11, 0.12])
 
-if False:
-    partnum = 1000
-    sim.show(gun, partnum)
-else:
-    partnum = 1e6
-    sim.simulate(gun, partnum)
+    if False:
+        partnum = 1000
+        sim.show(gun, partnum)
+    else:
+        partnum = 1e6
+        sim.simulate(gun, partnum)
 
-dtt0 = sim.gatherHistData('energy_gp0')
-mon0 = sim.gatherHistData('incident_energy')
-print('TOTAL: ', dtt0.getHit().sum())
+    dtt0 = sim.gatherHistData('energy_gp0')
+    mon1 = sim.gatherHistData('incident_energy')
+    mon2 = sim.gatherHistData('incident_energy_exit')
 
-# mon0.plot(show=True)
-dtt0.plot(show=True, log=False)
-# plt.plot(dtt0.getEdge()[:-1], dtt0.getWeight(), label='@detector')
-# plt.plot(mon0.getEdge()[:-1], mon0.getWeight(), label='@monitor')
-# plt.legend()
-# plotStyle()
-# plt.show()
+    print('TOTAL: ', dtt0.getHit().sum())
+    w_axi.append(w)
+    i_value.append(dtt0.getAccWeight())
+
+import pt_convolution2
+
+pt_convolution2.run()
+plt.plot(w_axi, i_value, marker='o', label='Prompt simulation')
+plt.grid()
+plt.legend()
+plt.show()
+    # mon1.plot(show=False)
+    # mon2.plot(show=False)
+    # dtt0.plot(show=True, log=False)
+    # plt.plot(dtt0.getEdge()[:-1], dtt0.getWeight(), label='@detector')
+    # plt.plot(mon0.getEdge()[:-1], mon0.getWeight(), label='@monitor')
+    # plt.legend()
+    # plotStyle()
+    # plt.show()
 
--- scripts/pt_convolution2.py
+++ scripts/pt_convolution2.py
@@ -14,15 +14,26 @@ sampleLength_z = 0.2 #cm
 sample_density_aa3 = 5 #per aa3
 sample_density_cm3 = sample_density_aa3 / (const_aa2cm**3) #n/aa3 / (cm/aa)^3 = n/cm^3
 
-def get_Rw(i_inp, w):
+rangel = 0.11
+rangeh = 0.12
+e_low = int((rangel - 0.) * 1000)
+e_high = int((rangeh - 0.) * 1000)
+
+def get_Rw(e_axi, i_inp, w):
     rw = 0
-    for j in range(len(i_inp)-1):
-        e_in = (e_axi[j+1] + e_axi[j]) * 0.5
-        e_out = e_in - w if e_in>w else e_in
-        prob_noCollide = np.exp(- getMacroXs(e_in, sample_density_cm3, e_out) * sampleLength_z)
-        # print(prob_not_interaction) 
-        rw += i_inp[j] * np.sqrt(e_out/e_in) * (1-prob_noCollide)
+    probs = []
+    for j in range(len(i_inp)):
+        e_in = e_axi[j]
+        e_out = e_in - w if e_in>w else 1e-99
+        prob_noCollide = get_prob_noCollide(e_in, e_out, sample_density_cm3, sampleLength_z)
+        probs.append(prob_noCollide)
+        rw += i_inp[j] * (1-prob_noCollide)
+    if w==0.08:
+        print(np.array(probs)[e_low : e_high].mean())
     return rw
+
+def get_prob_noCollide(ein, eout, density, z):
+    return np.exp(- getMacroXs(ein, density, eout) * z)
 
 def getMacroXs(e_in, density, e_out, Sw=1):
     microXs = np.sqrt(e_out/e_in) * Sw * 1e-24 #cm2
@@ -33,51 +44,51 @@ def moveAxis(axis, deviation):
         axis[i] = axis[i] + deviation
     return axis
 
-e_pulse = int((pulse_loc-0.)*1000)
-sw = unit_impulse(200, e_pulse)
-# print(sw)
+def run():
+    e_pulse = int((pulse_loc-0.)*1000)
+    sw = unit_impulse(200, e_pulse)
+    # print(sw)
 
-e_axi = np.arange(0,0.2,0.001)
-e_axi2 = np.zeros(len(e_axi))
+    e_axi = np.arange(0,0.2,0.001)
+    e_axi2 = np.zeros(len(e_axi))
 
-# incoming intensity
-i_inp = np.zeros(200)
-rw = np.zeros(200)
-e_low = int((0.11 - 0.) * 1000)
-e_high = int((0.12 - 0.) * 1000)
-i_ave = const_intensity_inp / (e_high-e_low)
-for i in range(e_low, e_high+1):
-    i_inp[i] = i_ave
-
+    # incoming intensity
+    i_inp = np.zeros(200)
+    rw = np.zeros(200)
+    i_ave = const_intensity_inp / (e_high-e_low)
+    for i in range(e_low, e_high):
+        i_inp[i] = i_ave
 
 
-w_axi = np.zeros(200)
-rw = np.zeros(100)
-for w in np.arange(0.,0.1,0.001):
-    index = int(w/0.001)
-    rw[index]=get_Rw(i_inp, w)
-w_axi2 = np.zeros(len(w_axi))
-# iw_scattered = unit_impulse(45, e_pulse) * const_intensity_inp
-# rw = np.zeros(len(iw))
-# rw = np.flip(iw)
-for j in range(len(e_axi)):
-    w_axi2[j] = -e_axi[-(j+1)]+pulse_loc
-# print(rw)
-cv = convolve(sw, rw, 'full')
 
-# plt.plot(e_axi, sw)
-plt.plot(e_axi, i_inp, label='Incident intensity I(E)')
-# plt.plot(e_axi2, rw)
-plt.plot(moveAxis(e_axi[1:101], 0.02), rw, label='Calculated by formulation R(w)')
-# plt.plot(e_axi, iw_scattered)
-# plt.plot(w_axi2, rw, label='Calculated Resolution function R(w)')
-cv_axi = np.arange(0, len(sw)+len(rw)-1) * 0.001 - 0.02
-plt.plot(cv_axi, cv, label='Convolution S*R(w)')
-# plt.plot(cv)
-plt.xticks(np.arange(-0.04,0.4,0.02))
-plt.grid()
-plt.legend()
-plt.show()
+    w_axi = np.zeros(200)
+    rw = np.zeros(100)
+    for w in np.arange(0.,0.1,0.001):
+        index = int(w/0.001)
+        rw[index]=get_Rw(e_axi, i_inp, w)
+    w_axi2 = np.zeros(len(w_axi))
+    # iw_scattered = unit_impulse(45, e_pulse) * const_intensity_inp
+    # rw = np.zeros(len(iw))
+    # rw = np.flip(iw)
+    for j in range(len(e_axi)):
+        w_axi2[j] = -e_axi[-(j+1)]+pulse_loc
+    # print(rw)
+    cv = convolve(sw, rw, 'full')
+
+    # plt.plot(e_axi, sw)
+    plt.plot(e_axi, i_inp, label='Incident intensity I(E)')
+    # plt.plot(e_axi2, rw)
+    plt.plot(moveAxis(e_axi[1:101], 0.02), rw, label='Calculated by formulation R(w)')
+    # plt.plot(e_axi, iw_scattered)
+    # plt.plot(w_axi2, rw, label='Calculated Resolution function R(w)')
+    cv_axi = np.arange(0, len(sw)+len(rw)-1) * 0.001 - 0.02
+    plt.plot(cv_axi, cv, label='Convolution S*R(w)')
+    # plt.plot(cv)
+    plt.xticks(np.arange(-0.04,0.4,0.02))
+    plt.xlabel('w(eV)')
+    plt.ylabel('I')
+
+    # plt.show()
 
 
 # a = np.array([0,1,0])
