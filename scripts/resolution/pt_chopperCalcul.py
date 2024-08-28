66098a2c031990ff726703a7c0dfb56103f308a1
panzy <zypan@ihep.ac.cn>
2024-08-28 07:28:26 +0000

    build resolution benchmark model

--- scripts/resolution/pt_chopperCalcul.py
+++ scripts/resolution/pt_chopperCalcul.py
@@ -2,7 +2,13 @@
 
 import numpy as np
 
-delta_t = 38 * 1e-6 #38us
+const_neutron_mass = 1.674e-27 #kg
+const_neutron_mass_amu = 1.00866491588
+const_c  = 299792458 # (m/s)/c
+const_dalton2eVc2 =  931494095.17
+const_neutron_mass_evc2 = const_neutron_mass_amu * const_dalton2eVc2  #[ eV/(Aa/s)^2 ]
+
+delta_t = 6 * 1e-6 #s
 
 theta_degree = 10
 theta=np.deg2rad(theta_degree) #rad
@@ -13,4 +19,35 @@ angle_velocity = theta / delta_t
 oneRoundtime = 2*np.pi/angle_velocity
 frequency = 1 / oneRoundtime
 
-print(f'theta_degree: {theta_degree}, frequency: {frequency}')
+distance_source_chopper = 10000 + 25000 #mm
+distance_source_chopper *= 1e-3 # convert to m
+
+def get_energy(velocity):
+    return 0.5 * const_neutron_mass_evc2 / const_c**2  * (velocity)**2
+
+def get_period(angle_velocity):
+    return 2 * np.pi / angle_velocity  #s
+
+def get_time_window(theta_rad, angle_velocity, order):
+    angle_window = np.array([-theta_rad/2, theta_rad/2])
+    period = get_period(angle_velocity)
+    time_window = angle_window / angle_velocity + period * order
+    return time_window
+
+def get_gun_energy_window(time_window, distance2Chopper):
+    velocity_window = distance2Chopper / time_window
+    energy_window = get_energy(velocity_window)
+    print(f'Time window: [{time_window[0]}, {time_window[1]}]; Unit: s')
+    print(f'Energy window: [{energy_window[1]}, {energy_window[0]}; Unit: eV\n')
+
+def time_flight2distance(time_window, distance2Chopper, distance2destination):
+    print(f'Neutron from chopper fly to destination time: {time_window * distance2destination / distance2Chopper}')
+    
+
+if __name__ == '__main__':
+    for i in np.arange(20):
+        print(f'theta_degree: {theta_degree}, frequency: {frequency}')
+        print(f'Order: {i}' )
+        tw = get_time_window(theta, angle_velocity, i)
+        # time_flight2distance(tw, distance_source_chopper, (10000+ 25000)*1e-3)
+        get_gun_energy_window(tw, distance_source_chopper)
--- scripts/resolution/pt_eResModel.py
+++ scripts/resolution/pt_eResModel.py
@@ -7,7 +7,7 @@
 from Cinema.Prompt import Prompt, PromptMPI
 from Cinema.Prompt.geo import Volume, Transformation3D
 from Cinema.Prompt.solid import Box,Tube, Sphere
-from Cinema.Prompt.scorer import DirectSqwHelper, MultiScatCounter
+from Cinema.Prompt.scorer import DirectSqwHelper, MultiScatCounter, TOFHelper, ESpectrumHelper
 from Cinema.Prompt.histogram import wl2ekin
 from Cinema.Prompt.physics import Material, Mirror
 from Cinema.Prompt.gun import IsotropicGun, PythonGun
@@ -21,31 +21,54 @@ class MySim(PromptMPI):
         super().__init__(seed)   
         self.mod_smp_dist = mod_smp_dist
         self.mean_ekin = mean_ekin
+        self.z_origin = 0 # sample position
+        self.y_monoChopper = self.y_pulseShaping = -17.5
+        self.z_pulseShaping = self.z_origin - 25000 - 1300
+        self.z_monoChopper = self.z_pulseShaping + 25000
 
     def makeWorld(self):
-        world = Volume("world", Box(2000, 2000, 21000))
+        world = Volume("world", Box(5000, 5000, 40000))
 
-        pulseShapingChopper = makeDiskChopper(20,15,0,1,731,10)
+        pulseShapingChopperVol = makeDiskChopper(20,15,0,1,731,10)
+        monochromaticChopperVol = makeDiskChopper(20,15,0,3, 4629.62962962963,5)
+
+        transformationPS = Transformation3D(0,self.y_pulseShaping,self.z_pulseShaping)
+        transformationMono = Transformation3D(0, self.y_monoChopper, self.z_monoChopper)
+        world.placeChild('pulseShapingChopperLV', pulseShapingChopperVol, transformationPS)
+        world.placeChild('monochromaticChopperLV', monochromaticChopperVol, transformationMono)
+
         # mat = 'physics=idealElaScat;xs_barn=1;density_per_aa3=5;energy_transfer_eV=0.03'
         mat = "physics=ncrystal;nccfg='bzscope_c1_vol_77K.ncmat;bragg=1';scatter_bias=10.0;abs_bias=1.0;"
         # mat = 'bzscope_c1_vol_77K.ncmat;bragg=0'
-        sample = Volume("sample", Sphere(0, 1), matCfg=mat)
+        sample = Volume("sample", Sphere(0, 1),matCfg=mat)
         ms = MultiScatCounter()
         ms.make(sample)
-        world.placeChild('samplePV', sample)
+        world.placeChild('samplePV', sample, Transformation3D(0,0,self.z_origin))
+
+        mon = Volume("mon", Box(50,50,1e-6))
+        tofScorerPS = TOFHelper("tofMonScorerPulseShaping", 0.005440, 0.0055, groupID=1)
+        tofScorerPS.make(mon)
+        tofScorerMono = TOFHelper("tofMonScorerMono", 0.0, 0.03, groupID=2)
+        tofScorerMono.make(mon)
+
+        engScorerBeforeSample = ESpectrumHelper("engMonBeforeSample", 0.01, 0.02,groupID=3)
+        engScorerBeforeSample.make(mon)
+
+        world.placeChild("tofMonPSLV", mon, Transformation3D(0,self.y_pulseShaping,self.z_pulseShaping+0.01), scorerGroup=1)
+        world.placeChild("tofMonMonoLV", mon, Transformation3D(0, self.y_monoChopper, self.z_monoChopper+0.01), scorerGroup=2)
+        world.placeChild("engMonBeforeSampleLV", mon, Transformation3D(0, 0, self.z_origin-5), scorerGroup=3)
 
         deg = np.pi/180.
 
-        monitor = Volume("mon", Sphere(1499.99, 1500, starttheta=1.*deg, deltatheta=178*deg ))
-        # monitor = Volume("mon", Box(10,10,2))
-        helper = DirectSqwHelper('sqw', self.mod_smp_dist, self.mean_ekin, [0,0,1.], [0,0,0],
-                 qmin = 1e-1, qmax = 12, num_qbin = 500, 
-                 ekinmin=-0.1, ekinmax=0.06,  num_ebin = 500,
-                 pdg = 2112, groupID  = 0, ptstate = 'ENTRY')
+        monitor = Volume("mon", Sphere(2999.99, 3000, starttheta=1.*deg, deltatheta=178*deg ))
+        # helper = DirectSqwHelper('sqw', self.mod_smp_dist, self.mean_ekin, [0,0,1.], [0,0,0],
+        #          qmin = 1e-1, qmax = 12, num_qbin = 500, 
+        #          ekinmin=-0.1, ekinmax=0.06,  num_ebin = 500,
+        #          pdg = 2112, groupID  = 0, ptstate = 'ENTRY')
+        helper = ESpectrumHelper("engScorerDetector", -0.1, 0.1,linear=True)
         helper.make(monitor)
         helper.addScatterCounter(ms, 1)
 
-        # world.placeChild('monPV', monitor,Transformation3D(0,1500,0).applyRotX(90))
         world.placeChild('monPV', monitor,Transformation3D(0,0,0))
 
         self.setWorld(world)
@@ -58,35 +81,60 @@ class MyGun(PythonGun):
         self.mod_smp_dist = mod_smp_dist
 
     def samplePosition(self):
-        return 0,0,-self.mod_smp_dist
+        return 0,0,self.mod_smp_dist
     
     def sampleEnergy(self):
-        if isinstance(self.ekin, list):
+        if isinstance(self.ekin, np.ndarray):
             r=np.random.uniform(self.ekin[0], self.ekin[1], 1)[0]
-            return r
+            return velocity2ekin(r)
         else:
             return self.ekin
 
-mod_smp_dist = 20000
-mean_ekin = 0.0253
+mod_smp_dist = 0 - 1300 - 25000 - 10000
+ekin = [0.017, 0.018]
 
-gun = MyGun(mod_smp_dist, mean_ekin)
+const_neutron_mass = 1.674e-27 #kg
+const_neutron_mass_amu = 1.00866491588
+const_c  = 299792458 # (m/s)/c
+const_dalton2eVc2 =  931494095.17
+const_neutron_mass_evc2 = const_neutron_mass_amu * const_dalton2eVc2  #[ eV/(Aa/s)^2 ]
 
-sim = MySim(mod_smp_dist, mean_ekin, seed=1010)
+def ekin2velocity(ekin):
+    return np.sqrt(2 * ekin / (const_neutron_mass_evc2 / const_c**2))
+
+def velocity2ekin(velocity):
+    return 0.5 * const_neutron_mass_evc2 / const_c**2  * (velocity)**2
+
+gun = MyGun(mod_smp_dist, ekin2velocity(np.array(ekin)))        # uniform time-of-flight
+
+sim = MySim(mod_smp_dist, 0.017532, seed=1010)
 sim.makeWorld()
 
 # sim.show(gun, 100)
 
-sim.simulate(gun, 1e9)
-res = sim.gatherHistData('sqw')
-destination = 0
-if sim.rank==destination:
-    # res.plot(False, log=False)
-    # res.plot(True, title=f'Total Weight: {res.getAccWeight()}', log=True)
-    fn = 'Si_bzscope_braggIncluded'
-    plt=res.plot(title=f'{fn}', log=True)
-    plt.xlabel(r'Q($\AA^{-1}$)')
-    plt.ylabel(r'$\omega$(eV)')
-    plt.tight_layout()
-    plt.grid(False)
-    plt.savefig(f'{fn}.pdf')
+sim.simulate(gun, 1e8)
+tofPS = sim.gatherHistData("tofMonScorerPulseShaping")
+tofMono = sim.gatherHistData("tofMonScorerMono")
+engBfSample = sim.gatherHistData("engMonBeforeSample")
+engDetector = sim.gatherHistData("engScorerDetector")
+if sim.rank==0:
+    print('Testing : ',tofMono.getTotalWeight())
+    print(f"Energy before sample: {(engBfSample.getEdge()[1:] * engBfSample.getWeight()).sum()/engBfSample.getAccWeight()}")
+    engBfSample.savefig('engBfSample.png')
+    tofPS.savefig('tofps.png')
+    tofMono.savefig('tofmono.png')
+    engDetector.savefig('engDetector.png')
+
+
+# res = sim.gatherHistData('sqw')
+# destination = 0
+# if sim.rank==destination:
+#     # res.plot(False, log=False)
+#     # res.plot(True, title=f'Total Weight: {res.getAccWeight()}', log=True)
+#     fn = 'Si_bzscope_braggIncluded'
+#     plt=res.plot(title=f'{fn}', log=True)
+#     plt.xlabel(r'Q($\AA^{-1}$)')
+#     plt.ylabel(r'$\omega$(eV)')
+#     plt.tight_layout()
+#     plt.grid(False)
+#     plt.savefig(f'{fn}.pdf')
