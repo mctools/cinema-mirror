find a major rotation bug. rot_new_x,y,z are not effective at all when vis in Transformation3D

scripts/px_ml_generator.py
@@ -1,102 +1,56 @@
-#!/usr/bin/env python3
-
-# /home/xxcai1/git/cinema/external/KDSource/install/bin/mcpltool -m merged test_killermcpl*
-# pymcpltool --stats --pdf merged.mcpl
-
-import numpy as np
-from Cinema.PiXiu.io.cell import QeXmlCell
-import phonopy
+# coding: utf-8
 import matplotlib.pyplot as plt
-import mcpl, os
 
-np.set_printoptions(suppress=True)
-from Cinema.PiXiu.phon import CohPhon
-
-# rundir ~/git/cinema/rundir/px/newNC/si
-
-resN = 1e7
-Qmesh = 100
-Qrange = 3
-# ## Data generation
-# Qx, Qy, Qz = (50, 50, 50)
-# x = np.linspace(0, 10, Qx)
-# y = np.linspace(0, 10, Qy)
-# z = np.linspace(0, 10, Qz)
-
-# xv, yv, zv = np.meshgrid(x, y, z)
-# Qpoints = np.stack((xv.flatten(), yv.flatten(), zv.flatten()), axis=-1)
-
-Qpoints = np.random.rand(Qmesh**3, 3)*Qrange
-ph = CohPhon(temperature=30)
-Qmag, en, Smag = ph.s(Qpoints)
-
-# plt.figure()
-# plt.hist(en.flatten(), bins=100)
-# plt.figure()
-# plt.hist(np.log(Smag.flatten()), range=[-200, 1], bins=100,log=True)
-# plt.show()
-
-# print(Smag)
-
-
-##  save to ssv
-import kdsource as kds
-pt = "n" 
-
-ssvfile = "samples.ssv"
-
-Es = np.atleast_2d(en.flatten()).T
-ws = Smag.flatten()
-
-# ws = np.ones_like(ws)
-# ws = np.atleast_2d(np.random.random(ws.size)).T
-
-N = Es.shape[0]
-dirs = np.zeros((N,3))
-dirs[:, 0] = 1
-poss = np.repeat(Qpoints, 6, axis=0)
-
-# Time
-ts = np.zeros((N,1))
-# Stack energies, positions, directions and times
-print('parts shape', Es.shape, poss.shape, dirs.shape, ts.shape)
-parts = np.concatenate((Es,poss,dirs,ts), axis=1)
-np.random.shuffle(parts)
-kds.savessv(pt, parts, ws, ssvfile)
-
-
-os.system("/home/caixx/git/cinema/external/KDSource/bin/ssv2mcpl samples.ssv samples.mcpl")
-os.system('pymcpltool --stats --pdf samples.mcpl.gz')
-os.system('mv mcpl.pdf mcpl_org.pdf')
-
-
-# fitting and resample
-#######################################################################################################################
+# https://kdepy.readthedocs.io/en/latest/kernels.html, kernels can be plot as 
+from KDEpy import NaiveKDE
+for name, func in NaiveKDE._available_kernels.items():
+   x, y = NaiveKDE(kernel=name).fit([0]).evaluate()
+   plt.plot(x, y, label=name + (' (F)' if func.finite_support else ''))
+plt.grid(True, ls='--', zorder=-15); plt.legend();
+plt.show()
+get_ipython().system('. ~/git/cinema/env.sh')
 import os
 import numpy as np
 import kdsource as kds
 import mcpl
 
-samples = "samples.mcpl.gz"
-N=mcpl.MCPLFile(samples).nparticles
+inputfile = "bl6.mcpl"
+cutoutput = "bl6_cut.mcpl"
+cutoff_eV = 10
+
+# !/home/xxcai1/git/cinema/cinemabin/bin/mcpl_cut $inputfile $output $cutoff_eV
+
+# !pymcpltool --stats --pdf $output
+# !mv mcpl.pdf mcpl_cut_res.pdf
+
+
+N=mcpl.MCPLFile(cutoutput).nparticles
 
 # PList: wrapper for MCPL file
-plist = kds.PList(samples)
+plist = kds.PList(cutoutput)
+# # Geometry: define metrics for variables
+# geom = kds.Geometry([kds.geom.Lethargy(cutoff_eV*1e-6),
+#                      kds.geom.SurfXY(),
+#                      kds.geom.Polar(),
+#                      kds.geom.Decade()])
+# var_importance = [5,1,1,3,3,3]
+
 # Geometry: define metrics for variables
-geom = kds.Geometry([kds.geom.Energy(),
-                     kds.geom.Vol(),
-                     kds.geom.Polar()])
+geom = kds.Geometry([kds.geom.Lethargy(cutoff_eV*1e-6),
+                     kds.geom.SurfXY(),
+                     kds.geom.Isotrop(),
+                     kds.geom.Decade()])
+var_importance = [5,1,1,3,3,3,3]
+
 # Create KDSource
-s = kds.KDSource(plist, geom, kernel='epa')
+s = kds.KDSource(plist, geom,  kernel='epa')
 
 # Give a little more importance to energy
-var_importance = [3,1,1,1,1,1]
+print(geom.varnames)
 
 parts,ws = s.plist.get(N=-1)
 scaling = s.geom.std(parts=parts)
 scaling /= var_importance
-
-
 # Method 3: Adaptive Maximum Likelihood Cross-Validation:
 # Creates a grid of adaptive bandwidths and evaluates the
 # cross-validation scores on each one, which is an indicator of the
@@ -110,35 +64,100 @@ batch_size = 10000 # Batch size for KNN search
 k = 10             # Numer of neighbors per batch
 s.fit(N, scaling=scaling, batch_size=batch_size, k=k)
 bw_knn = s.kde.bw
+bw_knn = s.kde.bw
 
 # MLCV optimization of previously calculated kNN bandwidth
 s.bw_method = "mlcv"
 N_cv = int(1E4)   # Use a smaller N to reduce computation times
 seed = bw_knn[:N_cv] # Use kNN BW as seed (first N elements)
-grid = np.logspace(-1,-0.4,10)
+grid = np.logspace(-1, -0.4,10)
 s.fit(N_cv, scaling=scaling, seed=seed, grid=grid)
 bw_cv = s.kde.bw
-
 # Extend MLCV optimization to full KNN BW
 bw_knn_cv = bw_knn * bw_cv[0]/bw_knn[0] # Apply MLCV factor
 dim = s.geom.dim
 bw_knn_cv *= kds.kde.bw_silv(dim,len(bw_knn))/kds.kde.bw_silv(dim,len(bw_cv)) # Apply Silverman factor
-
-# https://kdepy.readthedocs.io/en/latest/kernels.html, kernels can be plot as 
-# from KDEpy import NaiveKDE
-# for name, func in NaiveKDE._available_kernels.items():
-#    x, y = NaiveKDE(kernel=name).fit([0]).evaluate()
-#    plt.plot(x, y, label=name + (' (F)' if func.finite_support else ''))
-# plt.grid(True, ls='--', zorder=-15); plt.legend();
-# plt.show()
 
 s = kds.KDSource(plist, geom, bw=bw_knn_cv,  kernel='epa') # Create new KDSource with full BW
 s.fit(N=N, scaling=scaling)
 s.plot_t
 xmlfile = "source.xml"
 s.save(xmlfile) # Save KDSource to XML file
+resN=1000000
+resampled = "resampled"
+gzext = ".mcpl.gz"
 
-os.system(f'time /home/caixx/git/cinema/external/KDSource/bin/kdtool resample source.xml -o res -n {resN}')
-# os.system('mcpltool res.mcpl.gz -t res.txt')
-os.system('pymcpltool --stats --pdf res.mcpl.gz')
-os.system('mv mcpl.pdf mcpl_res.pdf')
+get_ipython().system('time /home/xxcai1/git/cinema/external/KDSource/install/bin/kdtool resample source.xml -o $resampled -n $resN')
+get_ipython().system('pymcpltool --stats --pdf resampled.mcpl.gz')
+get_ipython().system('mv mcpl.pdf mcpl_res.pdf')
+# os.system('')
+# KL divergence
+def kl_divergence(p, q):
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
+
+# Histogram of MCPL particle list
+def mcpl_hist(mcplfile, var, bins, part0=None, part1=None, **kwargs):
+    pl = mcpl.MCPLFile(mcplfile)
+    hist = np.zeros(len(bins)-1)
+    I = 0
+    for pb in pl.particle_blocks:
+        parts = np.stack((pb.ekin,pb.x,pb.y,pb.z,pb.ux,pb.uy,pb.uz,pb.time), axis=1)
+        mask1 = np.ones(len(parts), dtype=bool)
+        if part0 is not None:
+            mask1 = np.logical_and.reduce(part0 <= parts, axis=1)
+        mask2 = np.ones(len(parts), dtype=bool)
+        if part1 is not None:
+            mask2 = np.logical_and.reduce(parts <= part1, axis=1)
+        mask = np.logical_and(mask1, mask2)
+        data = parts[mask][:,var]
+        hist += np.histogram(data, bins=bins, weights=pb.weight[mask], **kwargs)[0]
+        I += np.sum(pb.weight)
+    hist /= I
+    hist /= (bins[1:]-bins[:-1])
+    return hist
+EE = np.logspace(-10,-5,100)
+
+fig,scores = s.plot_E(EE, label="KDE")
+
+widths = (EE[1:]-EE[:-1])
+
+hist = mcpl_hist('resampled.mcpl.gz', 0, EE)
+plt.bar(EE[:-1], hist, width=widths, align="edge", linewidth=.5, ec="k",
+        fc="g", alpha=.7, label="Resampled")
+
+hist = mcpl_hist('bl6_cut.mcpl', 0, EE)
+plt.bar(EE[:-1], hist, width=widths, align="edge", linewidth=.5, ec="k",
+        fc="r", alpha=.27, label="original")
+
+
+plt.legend()
+plt.tight_layout()
+plt.show()
+print(s.geom.varnames)
+tt = np.linspace(0,2,300)
+# fig,[scores,errs] = s.plot_integr("t", tt, yscale="linear")
+
+hist = mcpl_hist('resampled.mcpl.gz', 7, tt)
+plt.bar(tt[:-1], hist, width=np.diff(tt), align="edge", linewidth=.5, ec="k",
+        fc="g", alpha=.5, label="Resampled")
+
+
+hist = mcpl_hist(cutoutput, 7, tt)
+plt.bar(tt[:-1], hist, width=np.diff(tt), align="edge", linewidth=.5, ec="k",
+        fc="r", alpha=.2, label="original")
+plt.legend()
+# pb.ekin, pb.x, pb.y, pb.z, pb.ux, pb.uy,pb.uz, pb.time
+xx = np.linspace(20,45,20)
+yy = np.linspace(0,25,20)
+fig,scores = s.plot2D_integr(["x","y"], [xx,yy], scale="log")
+
+plt.clim(vmin=1e-5)
+plt.tight_layout()
+plt.show()
+xx = np.linspace(20,45,20)
+yy = np.linspace(0,25,20)
+fig,scores = s.plot2D_integr(["x","y"], [xx,yy], scale="lin")
+
+plt.clim(vmin=1e-5)
+plt.tight_layout()
+plt.show()

src/python/Cinema/Prompt/geo.py
@@ -46,12 +46,14 @@ _pt_ResourceManager_addSurface = importFunc('pt_ResourceManager_addSurface', Non
 _pt_ResourceManager_cfgVolPhysics = importFunc('pt_ResourceManager_cfgVolPhysics', None, [type_uint, type_cstr])
 
 class Transformation3D:
+    # fixme: rot_new_x=0., rot_new_z=0. rot_new_z are not effective at all when vis, why?
     def __init__(self, x=0., y=0., z=0., rot_z=0., rot_new_x=0., rot_new_z=0., degrees = True):
-        self.cobj = _pt_Transformation3D_newfromdata(x, y, z, 0, 0, 0, 1.,1.,1.)
+        self.cobj = _pt_Transformation3D_newfromdata(x, y, z, rot_z, rot_new_x, rot_new_z, 1.,1.,1.)
         self.sciRot = scipyRot.from_euler('ZXZ', [rot_z, rot_new_x, rot_new_z], degrees)
         self.translation = np.array([x, y, z])
-        self.sciRotMatrix = self.sciRot.as_matrix()
-    
+        # self.sciRotMatrix = self.sciRot.as_matrix()
+        self.py2cppConv()
+
     @property
     def euler(self):
         return self.sciRot.as_euler('xyz', True)
@@ -173,9 +175,10 @@ class Transformation3D:
         self.sciRot = scipyRot.from_euler('ZXZ', [rot_z, rot_new_x, rot_new_z], degrees)
         self.py2cppConv()
 
-    def transformInplace(self, input):
-        _pt_Transformation3D_transform(self.cobj, input.shape[0], input, input)
-        return input
+    def transform(self, input):
+        output = np.zeros_like(input)
+        _pt_Transformation3D_transform(self.cobj, input.shape[0], input, output)
+        return output
         
 class Volume:
     scorerDict = {}

src/pythontests/test_transf3d.py
@@ -30,10 +30,13 @@ for i in np.arange(10):
     mat1 = Transformation3D(rot_z=angles[0], rot_new_x=angles[1], rot_new_z=angles[2])
     mat2 = Transformation3D()
     mat2.setRot(rot_z=angles[0], rot_new_x=angles[1], rot_new_z=angles[2])
+    np.testing.assert_allclose(mat1.getRotMatrix(), mat2.getRotMatrix(), rtol=1e-15, atol=1e-15)
+
     data = np.random.random([1000,3])
     data2 = np.copy(data)
+
     np.testing.assert_allclose(data, data2, rtol=1e-15, atol=1e-15)
 
-    mat1.transformInplace(data)
-    mat2.transformInplace(data2)
-    np.testing.assert_allclose(data, data2, rtol=1e-15, atol=1e-15)
+    dataout = mat1.transform(data)
+    dataout2 = mat2.transform(data2)
+    np.testing.assert_allclose(dataout, dataout2, rtol=1e-15, atol=1e-15)