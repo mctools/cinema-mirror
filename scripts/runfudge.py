8edbb04a517049c97bee301a8748d6f5feafde54
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2024-12-02 02:01:51 +0000

    update runfudge script

--- scripts/runfudge.py
+++ scripts/runfudge.py
@@ -6,14 +6,15 @@ from multiprocessing import Pool
 import argparse
 
 class FileProcessor:
-    def __init__(self, dirname, output_dir, log_file='process.log', num_processes=40, temperatures=None, ext='endf'):
+    def __init__(self, calc, dirname, output_dir, log_file='process.log', num_processes=40, temperatures=None, ext='endf'):
+        self.calc = calc
         self.dirname = dirname + '/'
         self.output_dir = output_dir + '/'
         self.num_processes = num_processes
         self.ext = ext
         self.fn = []
         self.const_boltzmann = 8.6173303e-11
-        self.temp = np.array(temperatures) if temperatures else np.array([300])
+        self.temp = np.array(temperatures) if temperatures else []
         self.setup_logging(log_file)
         self.collect_files()
 
@@ -27,6 +28,7 @@ class FileProcessor:
             if files.endswith(self.ext):
                 self.fn.append(files)
         self.fn.sort()
+        print(self.fn)
         self.logger.info(f'Collected {len(self.fn)} files with extension {self.ext}')
 
     def run_file(self, f):
@@ -35,19 +37,27 @@ class FileProcessor:
                 self.logger.info("File TEM found. Exiting loop.")
                 return
 
-            # Run the first subprocess
-            result1 = subprocess.run(
-                f'endf2gnds.py --skipCovariances --skipBadData --ignoreBadDate --continuumSpectraFix {self.dirname+f} {self.output_dir+f+".xml"}',
-                shell=True, check=True, stderr=subprocess.PIPE, text=True)
+            if self.calc == 'endf':
+                result1 = subprocess.run(
+                    f'endf2gnds.py --skipCovariances --skipBadData --ignoreBadDate --continuumSpectraFix {self.dirname+f} {self.output_dir+f+".xml"}',
+                    shell=True, check=True, stderr=subprocess.PIPE, text=True)
 
-            substr = " "
-            for t in self.temp:
-                substr += ' -t ' + str(t * self.const_boltzmann) + '  '
+            elif self.calc == 'mc':
+                substr = " "
+                for t in self.temp:
+                    substr += ' -t ' + str(t * self.const_boltzmann) + '  '
 
-            # Run the second subprocess
-            result2 = subprocess.run(
-                f'processProtare.py {substr} -mc --hybrid {self.output_dir+f+".xml"} {self.output_dir+f+".gnds.xml"}',
-                shell=True, check=True, stderr=subprocess.PIPE, text=True)
+                # Run the second subprocess
+                result1 = subprocess.run(
+                    f'processProtare.py {substr} -mc --hybrid {self.dirname+f} {self.output_dir+f}',
+                    shell=True, check=True, stderr=subprocess.PIPE, text=True)
+
+            elif self.calc == 'urr':
+                file_name_without_extension, file_extension = os.path.splitext(f)
+                new_file_name = file_name_without_extension + ".urr" + file_extension
+                result1 = subprocess.run(
+                    f'processURR.py {self.output_dir+f} 10 --hybrid -o {self.output_dir+new_file_name}',
+                    shell=True, check=True, stderr=subprocess.PIPE, text=True)
 
             self.logger.info(f'{f} is done @ {self.temp} Kelvin')
             print(f'\033[92m{f} is done @ {self.temp} kelvin\033[0m')
@@ -63,17 +73,23 @@ class FileProcessor:
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process files in a directory with multiprocessing.')
-    parser.add_argument('-d', '--dirname', type=str, required=True, help='Directory containing input files.')
-    parser.add_argument('-o', '--output_dir', type=str, required=True, help='Directory for storing output files.')
-    parser.add_argument('-l', '--log_file', type=str, default='process.log', help='Log file name.')
-    parser.add_argument('-n', '--num_processes', type=int, default=40, help='Number of processes in the pool.')
-    parser.add_argument('-t', '--temperatures', type=float, nargs='+', default=[300], help='List of temperatures in Kelvin.')
-    parser.add_argument('-e', '--ext', type=str, default='endf', help='File extension to filter by.')
+    parser.add_argument('calc', action='store', type=str, default='', required=True,
+                          help='calculation type: endf, mc, urr.(default: %(default)s)')
+    parser.add_argument('-d', '--dirname', type=str, required=True, help='Directory containing input files. (default: %(default)s)')
+    parser.add_argument('-o', '--output_dir', type=str, required=True, help='Directory for storing output files. (default: %(default)s)')
+    parser.add_argument('-l', '--log_file', type=str, default='process.log', help='Log file name. (default: %(default)s)')
+    parser.add_argument('-n', '--num_processes', type=int, default=40, help='Number of processes in the pool. (default: %(default)s)')
+    parser.add_argument('-t', '--temperatures', type=float, nargs='+', default=[], help='List of temperatures in Kelvin. (default: %(default)s)')
+    parser.add_argument('-e', '--ext', type=str, required=True, help='File extension to filter by. (default: %(default)s)')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_arguments()
+    if args.calc not in ['endf', 'mc', 'urr']:
+        raise ValueError("Invalid calculation type. Use 'endf', 'mc', or 'urr'.")
+    
     processor = FileProcessor(
+        calc=args.calc,
         dirname=args.dirname,
         output_dir=args.output_dir,
         log_file=args.log_file,
--- src/cxx/Python/libsrc/PTPythonUtils.cc
+++ src/cxx/Python/libsrc/PTPythonUtils.cc
@@ -12,6 +12,6 @@ double pt_nccalNumDensity(const char *s)
 void pt_merge_mcpl(const char* file_output, unsigned nfiles, const char ** files)
 {
     mcpl_outfile_t fo = mcpl_merge_files( file_output, nfiles, files );
-    mcpl_closeandgzip_outfile(fo);
+    mcpl_close_outfile(fo);
     //fixme: consider mcpl_closeandgzip_outfile ??
 }
