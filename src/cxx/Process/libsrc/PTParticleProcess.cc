f3d5a37b5dc2959bd7d3583043febccf3c8b9daa
Xiao-Xiao Cai <caixx@ihep.ac.cn>
2025-02-20 12:36:05 +0800

    fix scatter_bias==0. bug as well

--- src/cxx/Process/libsrc/PTParticleProcess.cc
+++ src/cxx/Process/libsrc/PTParticleProcess.cc
@@ -283,7 +283,8 @@ void Prompt::ParticleProcess::cfgPhysicsModel(const std::string &cfgstr)
     else
     #endif
     {
-      m_discretModels->addPhysicsModel(std::make_shared<NCrystalScat>(nccfg, scatter_bias, 0));
+      if(scatter_bias)
+        m_discretModels->addPhysicsModel(std::make_shared<NCrystalScat>(nccfg, scatter_bias, 0));
       if(abs_bias)
         m_discretModels->addPhysicsModel(std::make_shared<NCrystalAbs>(nccfg, abs_bias, 0));
     }
