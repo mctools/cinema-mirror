445a429fa5c6f3e9edd546efdfdc0eef62dcd891
panzy <zypan@ihep.ac.cn>
2025-03-17 08:06:12 +0000

    fix

--- src/cxx/Python/libinc/PTPythonScorer.hh
+++ src/cxx/Python/libinc/PTPythonScorer.hh
@@ -39,7 +39,7 @@ void* pt_ScorerVolFluence_new(const char* name, double xmin, double xmax, unsign
 
 void* pt_ScorerMultiScat_new(const char* name, double xmin, double xmax, unsigned nbins, unsigned pdg, int groupid);
 
-void* pt_ScorerDeltaMomentum_new(const std::string &name,
+void* pt_ScorerDeltaMomentum_new(const char* name,
     double sourceSampleDist, double qmin, double qmax, unsigned numbin, unsigned int pdg,
     double mean_incident_dir_x, double mean_incident_dir_y, double mean_incident_dir_z,
     double sample_position_x, double sample_position_y, double sample_position_z, int type, 
