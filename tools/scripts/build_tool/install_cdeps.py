50beb662d3f15fa826a1f404e9b208e46d4cb371
panzy <zypan@ihep.ac.cn>
2025-05-09 08:17:13 +0000

    many fixes to make conda build working

--- tools/scripts/build_tool/install_cdeps.py
+++ tools/scripts/build_tool/install_cdeps.py
@@ -2,20 +2,25 @@
 """
 A python-based 3rd-party dependencies management for Prompt, intended to be cross-platform.
 """
-
-import os
+import os, sys
 import subprocess
 from pathlib import Path
-import wget
+import requests
 import shutil
 import platform
 import contextlib
 
+print(f'Pyproject side, using python at: {sys.executable}')
+
 git_domain = "https://code.ihep.ac.cn/cinema-developers/"
-kds_url = "kdsource/-/archive/v0.1.0_prompt/kdsource-v0.1.0_prompt.tar.gz"
+# kds_url = "kdsource/-/archive/v0.1.0_prompt/kdsource-v0.1.0_prompt.tar.gz"
 gidiplus_url = "gidiplus/-/archive/v3.28.22_prompt/gidiplus-v3.28.22_prompt.tar.gz"
 vecgeom_git = "https://code.ihep.ac.cn/caixx/VecGeom.git"
 vecgeom_branch = "cinema"
+
+kdsource_git = git_domain+"kdsource.git"
+kdsource_branch = "pt"
+
 cpu_count = os.cpu_count()
 
 src_root = Path(__file__).parent.parent.parent.parent
@@ -24,6 +29,48 @@ externalpath = src_root/"external"
 external_src_parent = externalpath/"src"
 external_build_path = externalpath/"build"
 install_root = externalpath/"install"
+# conda build will only bundle those in $PREFIX
+# to include kdsource when using conda build
+if os.environ.get("CONDA_BUILD"):
+    install_root_kds = os.environ.get("PREFIX")
+else:
+    install_root_kds = install_root
+
+def download_package(url, save_path):
+    dir = Path(save_path)
+    dir.mkdir(parents=True, exist_ok=True)
+    if platform.system() == 'Linux':
+        pkgname = str(url).split("/")[-1]
+    else:
+        pkgname = None
+        raise NotImplementedError("Only support platform Linux.")
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  
+        
+        with open(dir/pkgname, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:  
+                    f.write(chunk)
+        print(f"File downloaded to :{save_path}")
+        return str(dir/pkgname)
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Failed on download: {str(e)}")
+
+def config_deps_dir():
+    try:
+        result_nc = subprocess.run(["ncrystal-config", "--show", "cmakedir"], check=True, capture_output=True, text=True)
+        result_mcpl_lib = subprocess.run(["mcpl-config", "--show", "cmakedir"], check=True, capture_output=True, text=True)
+        result_mcpl_incdir = subprocess.run(["mcpl-config", "--show", "includedir"], check=True, capture_output=True, text=True)
+        prefix = Path(result_mcpl_incdir.stdout).parent
+        
+        return result_nc.stdout, result_mcpl_lib.stdout, result_mcpl_incdir.stdout, prefix
+    except Exception as e:
+        print(f"Error messages: ")
+        print(e.stderr)        
+
+ncdir, mcpldir, mcpl_incdir, prefix = config_deps_dir()
 
 def init_external_dir():
     if platform.system() == 'Linux':
@@ -43,11 +90,8 @@ def create_folder(path : Path):
     return path
 
 def download_from_url(git_domain, url, root: Path):
-    result = wget.download(git_domain+url, out=str(externalpath))
-    print("")
-    print(git_domain+url)
-    print("to ")
-    print(result)
+    result = download_package(git_domain+url, save_path=str(externalpath))
+    print(f"Download from {git_domain+url} to {result}")
     shutil.unpack_archive(result, str(root))
 
     extract_upper = Path(result).stem
@@ -64,9 +108,10 @@ def download(name, url=None, git_url=None, git_branch="main"):
         print("Using git repo: ", git_url)
         try:
             result_git = subprocess.run(["git", "clone", "-b", git_branch, git_url, name], cwd=str(external_src_parent),
-                                        capture_output=True,  text=True)
-        finally:
-            print(f"Error captured: {result_git.stderr}")
+                                        check=True, capture_output=True,  text=True)
+        except Exception as e:
+            print(f"Error messages: ")
+            print(e.stderr)
         src_path = external_src_parent/name
     else:
         print("Using url (released pkg): ", url)
@@ -75,12 +120,14 @@ def download(name, url=None, git_url=None, git_branch="main"):
 
 def install(name, cmakeargs=None, makeargs=None, url=None, git_url=None, git_branch="main"):
 
+    print(f"----------Processing {name} ------------")
     dep_root = download(name, url, git_url, git_branch)
 
     buildpath = create_folder(external_build_path/name)
     try:
         if not makeargs:
-            print("project uses cmake. ")
+            # print("project uses cmake. ")
+            print(["cmake"] + cmakeargs + [str(dep_root)], sep=" ")
             result = subprocess.run(["cmake"] + cmakeargs + [str(dep_root)], cwd=str(buildpath) ,
                                         check=True, capture_output=True,  text=True)
 
@@ -91,7 +138,7 @@ def install(name, cmakeargs=None, makeargs=None, url=None, git_url=None, git_bra
         elif makeargs and cmakeargs:
             raise TypeError("Only one of cmakeargs and makeargs should be provided.")
         elif makeargs and not cmakeargs:
-            print("project uses make. ", "cwd=", str(dep_root))
+            # print("project uses make. ", "cwd=", str(dep_root))
             # print(["make"]+makeargs, sep=',')
             result = subprocess.run(["make"]+makeargs, cwd=str(dep_root) , capture_output=True,  text=True)
         else:
@@ -101,47 +148,71 @@ def install(name, cmakeargs=None, makeargs=None, url=None, git_url=None, git_bra
         print(e.stderr)
 
 def prepare_dependencies():
-    vecgeom_cmakeargs = [f"-DCMAKE_INSTALL_PREFIX={install_root}" ,"-DVECGEOM_BUILTIN_VECCORE=ON" ,
-        "-DVECGEOM_FAST_MATH=OFF" , "-DBUILD_TESTING=OFF" ,"-DVECGEOM_GDML=ON" ,
-        "-DVECGEOM_USE_NAVINDEX=ON"]
+    vecgeom_cmakeargs = [
+        f"-DCMAKE_INSTALL_PREFIX={install_root}" ,
+        "-DVECGEOM_BUILTIN_VECCORE=ON" ,
+        "-DVECGEOM_FAST_MATH=OFF" ,
+        "-DBUILD_TESTING=OFF" ,
+        "-DVECGEOM_GDML=ON" ,
+        "-DVECGEOM_USE_NAVINDEX=ON"
+                         ]
     install("vecgeom", cmakeargs=vecgeom_cmakeargs, git_url=vecgeom_git, git_branch=vecgeom_branch)
 
-    kds_cmakeargs = [f"-DCMAKE_INSTALL_PREFIX={install_root}", "-DCMAKE_POLICY_VERSION_MINIMUM=3.24" ]
-    install("KDSource", cmakeargs=kds_cmakeargs, url=kds_url)
+    kds_cmakeargs = [
+        f"-DCMAKE_INSTALL_PREFIX={install_root_kds}", 
+        "-DCMAKE_POLICY_VERSION_MINIMUM=3.24", 
+        f"-DCMAKE_PREFIX_PATH={prefix}"
+        ]
+    install("KDSource", cmakeargs=kds_cmakeargs, git_url=kdsource_git, git_branch=kdsource_branch)
 
-    gidiplus_makeargs = ["-s", "install4prompt", f"-j{cpu_count//2}", 'SHELL=bash', 'CXXFLAGS=-std=c++11 -fPIC', 'CFLAGS=-fPIC', # fixme: dynamic SHELL
-                         f"INSTALL_PREFIX={install_root}"]
+    # fixme: dynamic SHELL
+    gidiplus_makeargs = [
+        "-s", 
+        "install4prompt", 
+        f"-j{cpu_count//2}", 
+        'SHELL=bash', 
+        'CXXFLAGS=-std=c++11 -fPIC', 
+        'CFLAGS=-fPIC', 
+        f"INSTALL_PREFIX={install_root}"
+        ]
     install("gidiplus", makeargs=gidiplus_makeargs, url=gidiplus_url)
+
+def copy_sharedlib(pathfrom: Path, pathto: Path):
+    pathfrom = pathfrom.resolve()
+    pathto = pathto.resolve()
+    shutil.copy2(str(pathfrom), str(pathto))
 
 def main_build():
     create_folder(src_build_path)
     lib_path = install_root/"lib"
     veccore_dir = install_root/"lib"/"cmake"/"VecCore"
-    cinema_cmakeargs = [f"-DCMAKE_PREFIX_PATH={install_root}", f"-DVecCore_DIR={veccore_dir}", f"-DKDS_LIB={lib_path}",
-                        f"-DGIDIPLUS_LIB={lib_path}", f"-DGIDIPLUS_INCDIR={lib_path}", ]
+    cinema_cmakeargs = [
+        f"-DCMAKE_PREFIX_PATH={install_root};{prefix}", 
+        f"-DVecCore_DIR={veccore_dir}", 
+        f"-DKDS_LIB={lib_path}",
+        f"-DGIDIPLUS_LIB={lib_path}", 
+        f"-DGIDIPLUS_INCDIR={lib_path}", 
+        f"-DMCPL_DIR={mcpldir}"
+        ]
     result = subprocess.run(["cmake"]+cinema_cmakeargs+[".."], cwd=str(src_build_path), capture_output=True,  text=True)
-    print(result.stdout)
-    print(result.stderr)
     result = subprocess.run(["make", f"-j{cpu_count}"], cwd=str(src_build_path), capture_output=True,  text=True)
-    print(result.stdout)
-    print(result.stderr)
 
     sharedlib_from = src_build_path/"src"/"cxx"/"libprompt_core.so"
-    sharedlib_from = sharedlib_from.resolve()
     sharedlib_to = Path("src/python/Cinema")
-    sharedlib_to = sharedlib_to.resolve()
-
-    shutil.copy2(str(sharedlib_from), str(sharedlib_to))
-
+    copy_sharedlib(sharedlib_from, sharedlib_to)
 
 def main():
-    print("Show nc")
-    subprocess.run(["ncrystal-config", "--show", "cmakedir"])
-    subprocess.run(["mcpl-config", "--show", "cmakedir"])
-    init_external_dir()
-    prepare_dependencies()
-    main_build()
+    libpath = src_build_path/"src"/"cxx"/"libprompt_core.so"
+    # if libpath.exists():
+    #     print("Aleary build. Skip. Remove build files to rebuild")
+    #     return 0
+    try:
+        init_external_dir()
+        prepare_dependencies()
+        main_build()
+    except Exception as e:
+        print(f"Error messages: ")
+        print(e.stderr)
 
-    
 if __name__ == "__main__":
     main()
